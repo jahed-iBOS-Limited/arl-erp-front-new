03d0820c245fe7d859d3f3abc465c66e326f2732
mahmudhasan <mahmudhasan.akij@gmail.com>
2023-07-08 18:53:02 +0600

    receivable report added in CUSTOMER BALANCE DAYS & LIMIT and some code refactoring

--- src/app/modules/salesManagement/report/customerBalanceDayNLimit/landing/helper.js
+++ src/app/modules/salesManagement/report/customerBalanceDayNLimit/landing/helper.js
@@ -1,40 +1,53 @@
-export const reportId = {
-  first: `bb2df915-a2f8-47d8-a39d-906a547bb852`,
-  second: `b9092f9e-962d-4b87-80a3-29292cb1f579`,
+export const getReportId = (values) => {
+  const typeId = values?.reportType?.value;
+  const regularIrregularParty = `bb2df915-a2f8-47d8-a39d-906a547bb852`;
+  const sisterConcernOverdue = `b9092f9e-962d-4b87-80a3-29292cb1f579`;
+  const receivableReport = `d2ee852a-f129-4167-87f3-3ef0c00c9f8c`;
+  const reportId =
+    typeId === 2
+      ? regularIrregularParty
+      : typeId === 3
+      ? sisterConcernOverdue
+      : typeId === 4
+      ? receivableReport
+      : "";
+  return reportId;
 };
 
-export const groupId = {
-  first: `e3ce45bb-e65e-43d7-9ad1-4aa4b958b29a`,
-  second: `e3ce45bb-e65e-43d7-9ad1-4aa4b958b29a`,
-};
+export const groupId = "e3ce45bb-e65e-43d7-9ad1-4aa4b958b29a";
 
-export const parameterValues = (values, selectedBusinessUnit, profileData) => {
-  return [
-    { name: "intunit", value: selectedBusinessUnit?.value?.toString() },
+export const parameterValues = (values, buId, employeeId) => {
+  const typeId = values?.reportType?.value;
+
+  const regularIrregularParty = [
+    { name: "intunit", value: buId?.toString() },
     {
       name: "TransactionDate",
       value: values?.date,
     },
-    { name: "customerId", value: values?.customer?.value?.toString() || "0" },
+    {
+      name: "customerId",
+      value: values?.customer?.value?.toString() || "0",
+    },
     {
       name: "intchannelid",
       value: values?.distributionChannel?.value?.toString() || "0",
     },
-    { name: "intEmployeeid", value: profileData?.employeeId?.toString() },
+    { name: "intEmployeeid", value: employeeId?.toString() },
     { name: "region", value: "0" },
     { name: "area", value: "0" },
     { name: "territory", value: "0" },
   ];
-};
-
-export const parameterValuesTwo = (values, selectedBusinessUnit) => {
-  return [
-    { name: "intunit", value: selectedBusinessUnit?.value?.toString() },
+  const sisterConcernOverdue = [
+    { name: "intunit", value: buId?.toString() },
     {
       name: "TransactionDate",
       value: values?.date,
     },
-    { name: "customerId", value: values?.customer?.value?.toString() || "0" },
+    {
+      name: "customerId",
+      value: values?.customer?.value?.toString() || "0",
+    },
     {
       name: "intchannelid",
       value: values?.distributionChannel?.value?.toString() || "0",
@@ -42,4 +55,51 @@ export const parameterValuesTwo = (values, selectedBusinessUnit) => {
     { name: "intLevelId", value: "0" },
     { name: "RATId", value: "0" },
   ];
+  const receivableReport = [
+    { name: "intunit", value: buId?.toString() },
+    {
+      name: "TransactionDate",
+      value: values?.date,
+    },
+    {
+      name: "intpartid",
+      value: values?.viewType?.value?.toString() || "0",
+    },
+  ];
+
+  const parameters =
+    typeId === 2
+      ? regularIrregularParty
+      : typeId === 3
+      ? sisterConcernOverdue
+      : typeId === 4
+      ? receivableReport
+      : "";
+
+  return parameters;
 };
+
+export // Table Header
+const dayThs = [
+  "Sl",
+  "Partner Code",
+  "Partner Name",
+  "Ledger Balance",
+  "Credit Days",
+  "Permanent Credit Limit",
+  "Short Time Credit Limit",
+  "Total Credit Limit",
+  "Sales Amount",
+  "Deposit Amount",
+  "Limit Base Overdue",
+  "Days Base Overdue",
+  "Region",
+  "Area",
+  "Territory",
+  "Last Delivery Date",
+  "Last Payment Date",
+  "Product Delivery Gap",
+  "Payment Gap",
+  // "Delivery Date Difference",
+  // "Collection Date Difference",
+];
