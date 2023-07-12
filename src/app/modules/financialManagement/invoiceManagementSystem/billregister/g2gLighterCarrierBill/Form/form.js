75f0b06b6270fa03e03387fd137fc19756115d0a
mahmudhasan <mahmudhasan.akij@gmail.com>
2023-07-12 17:03:30 +0600

    g2g lighter bill from bill register is done

---
+++ src/app/modules/financialManagement/invoiceManagementSystem/billregister/g2gLighterCarrierBill/Form/form.js
@@ -0,0 +1,326 @@
+import { Form, Formik } from "formik";
+import React from "react";
+import { useDispatch } from "react-redux";
+import * as Yup from "yup";
+import { _dateFormatter } from "../../../../../_helper/_dateFormate";
+import { _fixedPoint } from "../../../../../_helper/_fixedPoint";
+import IView from "../../../../../_helper/_helperIcons/_view";
+import InputField from "../../../../../_helper/_inputField";
+import { getDownlloadFileView_Action } from "../../../../../_helper/_redux/Actions";
+import PaginationSearch from "../../../../../_helper/_search";
+import NewSelect from "../../../../../_helper/_select";
+import AttachFile from "../../../../../_helper/commonInputFieldsGroups/attachemntUpload";
+import FromDateToDateForm from "../../../../../_helper/commonInputFieldsGroups/dateForm";
+import useAxiosGet from "../../../../../_helper/customHooks/useAxiosGet";
+import { PortAndMotherVessel } from "../../../../../vesselManagement/common/components";
+import IButton from "../../../../../_helper/iButton";
+
+const validationSchema = Yup.object().shape({
+  billNo: Yup.string().required("Bill No is Required"),
+  billDate: Yup.date().required("Bill Date is Required"),
+  paymentDueDate: Yup.date().required("Payment Date is Required"),
+});
+
+export default function _Form({
+  buId,
+  btnRef,
+  getData,
+  initData,
+  gridData,
+  saveHandler,
+  setGridData,
+  resetBtnRef,
+  setUploadedImage,
+}) {
+  const [open, setOpen] = React.useState(false);
+  const [lighterCarrierDDL, getLighterCarrierDDL] = useAxiosGet();
+  const dispatch = useDispatch();
+  return (
+    <>
+      <Formik
+        enableReinitialize={true}
+        initialValues={initData}
+        validationSchema={validationSchema}
+        onSubmit={(values, { setSubmitting, resetForm }) => {
+          saveHandler(values, () => {
+            resetForm(initData);
+            setGridData([]);
+          });
+        }}
+      >
+        {({
+          handleSubmit,
+          resetForm,
+          values,
+          errors,
+          touched,
+          setFieldValue,
+        }) => (
+          <>
+            <Form className="form form-label-right">
+              <div className="row global-form">
+                <div className="col-12">
+                  <div className="row align-items-end">
+                    <PortAndMotherVessel
+                      obj={{
+                        values,
+                        setFieldValue,
+                        onChange: (fieldName, allValues) => {
+                          if (fieldName === "port") {
+                            getLighterCarrierDDL(
+                              `/wms/FertilizerOperation/GetLighterCarrierDDL?BusinessUnitId=${buId}&PortId=${allValues?.port?.value}`
+                            );
+                          }
+                        },
+                      }}
+                    />
+                    <div className="col-lg-3">
+                      <NewSelect
+                        label="Carrier Name"
+                        placeholder="Carrier Name"
+                        value={values?.carrierName}
+                        name="carrierName"
+                        options={
+                          lighterCarrierDDL?.map((itm) => ({
+                            ...itm,
+                            value: itm?.carrierId,
+                            label: itm?.carrierName,
+                          })) || []
+                        }
+                        onChange={(e) => {
+                          setFieldValue("carrierName", e);
+                        }}
+                        isDisabled={false}
+                      />
+                    </div>
+
+                    <FromDateToDateForm obj={{ values, setFieldValue }} />
+                    <IButton
+                      colSize={"col-lg-3"}
+                      onClick={() => {
+                        setGridData([]);
+                        getData(values, "");
+                      }}
+                      disabled={!values?.motherVessel || !values?.carrierName}
+                    />
+                  </div>
+                  {gridData?.length > 0 && (
+                    <>
+                      <div className="row">
+                        <div className="col-3">
+                          <InputField
+                            value={values?.billNo}
+                            label="Bill No"
+                            name="billNo"
+                            placeholder="Bill No"
+                          />
+                        </div>
+                        <div className="col-3">
+                          <InputField
+                            value={_dateFormatter(values?.billDate)}
+                            label="Bill Date"
+                            type="date"
+                            name="billDate"
+                            placeholder="Bill Date"
+                          />
+                        </div>
+                        <div className="col-3">
+                          <InputField
+                            value={_dateFormatter(values?.paymentDueDate)}
+                            label="Payment Due Date"
+                            type="date"
+                            name="paymentDueDate"
+                            placeholder="Payment Due Date"
+                          />
+                        </div>
+                      </div>
+                      <div className="row align-items-end">
+                        <div className="col-9">
+                          <InputField
+                            value={values?.narration}
+                            label="Narration No"
+                            name="narration"
+                            placeholder="Narration"
+                          />
+                        </div>
+                        <div className="col-3">
+                          <div className="row align-items-end">
+                            <div className="col-5">
+                              <button
+                                className="btn btn-primary"
+                                type="button"
+                                onClick={() => setOpen(true)}
+                              >
+                                Attachment
+                              </button>
+                              {values?.attachmentId && (
+                                <IView
+                                  classes="purchaseInvoiceAttachIcon"
+                                  clickHandler={() => {
+                                    dispatch(
+                                      getDownlloadFileView_Action(
+                                        values?.attachmentId
+                                      )
+                                    );
+                                  }}
+                                />
+                              )}
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </>
+                  )}
+                </div>
+              </div>
+
+              <button
+                type="submit"
+                style={{ display: "none" }}
+                ref={btnRef}
+                onSubmit={() => handleSubmit()}
+              ></button>
+              <button
+                type="reset"
+                style={{ display: "none" }}
+                ref={resetBtnRef}
+                onSubmit={() => resetForm(initData)}
+              ></button>
+            </Form>
+            <div className="row mt-1 ">
+              <div
+                className="col d-flex justify-content-between"
+                style={{
+                  fontSize: "15px",
+                  fontWeight: "bold",
+                }}
+              >
+                <div>
+                  <PaginationSearch
+                    placeholder="Search"
+                    paginationSearchHandler={(search) =>
+                      getData(values, search)
+                    }
+                  />
+                </div>
+                <p>
+                  Total Qty:{" "}
+                  {_fixedPoint(
+                    gridData?.reduce(
+                      (a, b) =>
+                        Number(a) +
+                        (b.checked ? Number(b.surveyQuantity || 0) : 0),
+                      0
+                    ),
+                    true
+                  )}
+                </p>
+                <p>
+                  Total Amount:{" "}
+                  {_fixedPoint(
+                    gridData?.reduce(
+                      (a, b) =>
+                        Number(a) +
+                        (b.checked ? Number(b.carrierTotalAmount || 0) : 0),
+                      0
+                    ),
+                    true
+                  )}
+                </p>
+              </div>
+
+              <div className="table-responsive">
+                <table className="table table-striped table-bordered global-table">
+                  <thead className="bg-secondary">
+                    <tr>
+                      <th>
+                        <input
+                          type="checkbox"
+                          checked={
+                            gridData?.length > 0
+                              ? gridData?.every((item) => item?.checked)
+                              : false
+                          }
+                          onChange={(e) => {
+                            setGridData(
+                              gridData?.map((item) => {
+                                return {
+                                  ...item,
+                                  checked:
+                                    item?.transportRate <= 0
+                                      ? false
+                                      : e?.target?.checked,
+                                };
+                              })
+                            );
+                          }}
+                        />
+                      </th>
+                      <th>SL</th>
+                      <th>Mother Vessel</th>
+                      <th>Lighter Vessel</th>
+                      <th>Lighter Destination</th>
+                      <th>Carrier Agent Name</th>
+                      <th>Quantity</th>
+                      <th>Rate</th>
+                      <th>Bill Amount</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {gridData?.map((item, index) => (
+                      <tr
+                        key={index}
+                        style={
+                          item?.transportRate <= 0
+                            ? { backgroundColor: "#ff000082" }
+                            : {}
+                        }
+                      >
+                        <td className="text-center align-middle">
+                          <input
+                            type="checkbox"
+                            // value = {item?.checked ? true:false}
+                            checked={item?.checked}
+                            onChange={(e) => {
+                              item["checked"] = e.target.checked;
+                              setGridData([...gridData]);
+                            }}
+                            disabled={item?.transportRate <= 0}
+                          />
+                        </td>
+                        <td className="text-center align-middle">
+                          {index + 1}
+                        </td>
+                        <td>{item?.motherVesselName}</td>
+                        <td>{item?.lighterVesselName}</td>
+                        <td>{item?.lighterDestinationName}</td>
+                        <td>{item?.carrierAgentName}</td>
+
+                        <td className="text-right">{item?.surveyQuantity}</td>
+                        <td className="text-right">{item?.carrierRate}</td>
+                        <td style={{ width: "100px" }}>
+                          <InputField
+                            value={item?.carrierTotalAmount}
+                            name="carrierTotalAmount"
+                            placeholder="Total Amount"
+                            type="number"
+                            onChange={(e) => {
+                              item.carrierTotalAmount = e.target.value;
+                              setGridData([...gridData]);
+                            }}
+                          />
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+
+            <AttachFile obj={{ open, setOpen, setUploadedImage }} />
+          </>
+        )}
+      </Formik>
+    </>
+  );
+}
