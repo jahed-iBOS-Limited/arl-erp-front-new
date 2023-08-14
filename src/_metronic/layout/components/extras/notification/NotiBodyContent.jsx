d8ac124c695d91d7dcd191b574a60ebc7bf98d1d
jahedhossain <jahed.w.dev@gmail.com>
2023-08-14 19:03:00 +0600

    fix issue

--- src/_metronic/layout/components/extras/notification/NotiBodyContent.jsx
+++ src/_metronic/layout/components/extras/notification/NotiBodyContent.jsx
@@ -55,7 +55,7 @@ const NotiBodyContent = ({ content, orgId, buId, handleClose, setLoading }) => {
             };
             policyFileUrl();
           }
-          NotificationRirectFunc({
+          notificationMaster?.routeUrl && NotificationRirectFunc({
             ...notificationMaster,
             peopledeskApiURL: peopledeskApiURL,
           })
