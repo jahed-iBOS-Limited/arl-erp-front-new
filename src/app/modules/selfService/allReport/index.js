6479126cd49e2362b57bce112e31998744792f4e
jahedhossain <jahed.w.dev@gmail.com>
2023-09-06 19:05:27 +0600

    allReport: manu add

--- src/app/modules/selfService/allReport/index.js
+++ src/app/modules/selfService/allReport/index.js
@@ -1,11 +1,11 @@
 import { Form, Formik } from "formik";
 import { TablePagination } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
-import ICustomCard from "./../_helper/_customCard";
 import AllReportSearchInput from "./allReportSearchInput";
 import { allSheetData } from "./data";
 import "./style.scss";
 import { convertKeysSpace, searchMatch } from "./utility";
+import ICustomCard from './../../_helper/_customCard';
 function AllReport() {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(15);
@@ -67,7 +67,7 @@ function AllReport() {
             <>
               <Form className='form form-label-left'>
                 <div className='row'>
-                  <div className='col-lg-12'>
+                  <div className='col-lg-9'>
                     <AllReportSearchInput
                       placeholder='Report Search'
                       paginationSearchHandler={(value) => {
@@ -115,7 +115,9 @@ function AllReport() {
                                       window.open(itm?.[`${url}`], "_blank");
                                     }}
                                   >
-                                    <sapn className="link">{itm?.[`${url}`]}</sapn>
+                                    <sapn className='link'>
+                                      {itm?.[`${url}`]}
+                                    </sapn>
                                   </td>
                                   <td>{itm?.process}</td>
                                   <td>{itm?.datasource}</td>
@@ -126,48 +128,6 @@ function AllReport() {
                         </tbody>
                       </table>
                     </div>
-                    {/* <ul className='list-group'>
-                      {(rowsPerPage > 0
-                        ? renderList.slice(
-                            page * rowsPerPage,
-                            page * rowsPerPage + rowsPerPage
-                          )
-                        : renderList
-                      ).map((itm, idx) => {
-                        let url = `reportformatlink(url)`;
-                        let sl = page * rowsPerPage + idx + 1;
-                        return (
-                          <>
-                            <li>
-                              <p>
-                                {" "}
-                                <span>{sl}) {' '}</span>
-                                {itm?.standardreportname}
-                              </p>
-                              <div className='buttomBox'>
-                                {itm?.[`${url}`] && (
-                                  <div
-                                    className='box link'
-                                    onClick={() => {
-                                      // new tab open
-                                      window.open(itm?.[`${url}`], "_blank");
-                                    }}
-                                  >
-                                    {itm?.[`${url}`]}
-                                  </div>
-                                )}
-                                {itm?.process && (
-                                  <div className='box'>{itm?.process}</div>
-                                )}
-                                {itm?.datasource && (
-                                  <div className='box'>{itm?.datasource}</div>
-                                )}
-                              </div>
-                            </li>
-                          </>
-                        );
-                      })}
-                    </ul> */}
                   </div>
                 </div>
                 <div className='row'>
@@ -203,3 +163,53 @@ function AllReport() {
 }
 
 export default AllReport;
+
+// <>
+//   <ul className='list-group'>
+//     {(rowsPerPage > 0
+//       ? renderList.slice(
+//           page * rowsPerPage,
+//           page * rowsPerPage + rowsPerPage
+//         )
+//       : renderList
+//     ).map((itm, idx) => {
+//       let url = `reportformatlink(url)`;
+//       let sl = page * rowsPerPage + idx + 1;
+//       return (
+//         <>
+//           <li>
+//             <p>
+//               {" "}
+//               <span>{sl}) </span>
+//               {itm?.standardreportname}
+//             </p>
+//             <div className='buttomBox'>
+//               {itm?.[`${url}`] && (
+//                 <div
+//                   className='box link'
+//                   onClick={() => {
+//                     // new tab open
+//                     window.open(
+//                       itm?.[`${url}`],
+//                       "_blank"
+//                     );
+//                   }}
+//                 >
+//                   {itm?.[`${url}`]}
+//                 </div>
+//               )}
+//               {itm?.process && (
+//                 <div className='box'>{itm?.process}</div>
+//               )}
+//               {itm?.datasource && (
+//                 <div className='box'>
+//                   {itm?.datasource}
+//                 </div>
+//               )}
+//             </div>
+//           </li>
+//         </>
+//       );
+//     })}
+//   </ul>
+// </>
