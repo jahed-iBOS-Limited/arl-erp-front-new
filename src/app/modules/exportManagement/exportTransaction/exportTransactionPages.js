5ec9e00ec618eada71fc5c4bcbd94d353f38bb1c
mahmudhasan <mahmudhasan.akij@gmail.com>
2023-09-10 15:59:46 +0600

    job order for export is seperated

--- src/app/modules/exportManagement/exportTransaction/exportTransactionPages.js
+++ src/app/modules/exportManagement/exportTransaction/exportTransactionPages.js
@@ -10,73 +10,80 @@ import SalesContractCreateEdit from "./salesContract/createEdit";
 import SalesOrderLanding from "./salesOrder";
 import SalesOrderCreateEdit from "./salesOrder/createEdit";
 import CreateEditJobOrder from "./salesOrder/jobOrder/createEditJobOrder";
+import JobOrderLanding from "./jobOrder/landing";
 
 export function ExportTransactionPages() {
     return (
-        <Switch>
-            <Redirect
-                exact={true}
-                from="/managementExport/exptransaction"
-                to="/managementExport/exptransaction/salescontract"
-            />
-            {/* sales contract */}
-            <ContentRoute
-                path="/managementExport/exptransaction/salescontract/edit/:id"
-                component={SalesContractCreateEdit}
-            />
-            <ContentRoute
-                path="/managementExport/exptransaction/salescontract/create"
-                component={SalesContractCreateEdit}
-            />
-            <ContentRoute
-                path="/managementExport/exptransaction/salescontract"
-                component={SalesContractLanding}
-            />
-            {/* commercial invoice */}
-            <ContentRoute
-                path="/managementExport/exptransaction/commercialinvoice/edit/:id"
-                component={CommercialInvoiceCreateEdit}
-            />
-            <ContentRoute
-                path="/managementExport/exptransaction/commercialinvoice/create"
-                component={CommercialInvoiceCreateEdit}
-            />
-            <ContentRoute
-                path="/managementExport/exptransaction/commercialinvoice"
-                component={CommercialInvoiceLanding}
-            />
-            {/* job order */}
-            <ContentRoute
-                path="/managementExport/exptransaction/salesorder/jobOrder/:id"
-                component={CreateEditJobOrder}
-            />
-            {/* sales order */}
-            <ContentRoute
-                path="/managementExport/exptransaction/salesorder/edit/:id"
-                component={SalesOrderCreateEdit}
-            />
-            <ContentRoute
-                path="/managementExport/exptransaction/salesorder/create"
-                component={SalesOrderCreateEdit}
-            />
-            <ContentRoute
-                path="/managementExport/exptransaction/salesorder"
-                component={SalesOrderLanding}
-            />
-            {/* packing and weight list */}
-            <ContentRoute
-                path="/managementExport/exptransaction/packingnweightlist/edit/:id"
-                component={PackingAndWeightListCreateEdit}
-            />
-            <ContentRoute
-                path="/managementExport/exptransaction/packingnweightlist/create"
-                component={PackingAndWeightListCreateEdit}
-            />
-            <ContentRoute
-                path="/managementExport/exptransaction/packingnweightlist"
-                component={PackingAndWeightListLanding}
-            />
-        </Switch>
+      <Switch>
+        <Redirect
+          exact={true}
+          from="/managementExport/exptransaction"
+          to="/managementExport/exptransaction/salescontract"
+        />
+        {/* sales contract */}
+        <ContentRoute
+          path="/managementExport/exptransaction/salescontract/edit/:id"
+          component={SalesContractCreateEdit}
+        />
+        <ContentRoute
+          path="/managementExport/exptransaction/salescontract/create"
+          component={SalesContractCreateEdit}
+        />
+        <ContentRoute
+          path="/managementExport/exptransaction/salescontract"
+          component={SalesContractLanding}
+        />
+        {/* commercial invoice */}
+        <ContentRoute
+          path="/managementExport/exptransaction/commercialinvoice/edit/:id"
+          component={CommercialInvoiceCreateEdit}
+        />
+        <ContentRoute
+          path="/managementExport/exptransaction/commercialinvoice/create"
+          component={CommercialInvoiceCreateEdit}
+        />
+        <ContentRoute
+          path="/managementExport/exptransaction/commercialinvoice"
+          component={CommercialInvoiceLanding}
+        />
+        {/* job order */}
+        <ContentRoute
+          path="/managementExport/exptransaction/salesorder/jobOrder/:id"
+          component={CreateEditJobOrder}
+        />
+        {/* sales order */}
+        <ContentRoute
+          path="/managementExport/exptransaction/salesorder/edit/:id"
+          component={SalesOrderCreateEdit}
+        />
+        <ContentRoute
+          path="/managementExport/exptransaction/salesorder/create"
+          component={SalesOrderCreateEdit}
+        />
+        <ContentRoute
+          path="/managementExport/exptransaction/salesorder"
+          component={SalesOrderLanding}
+        />
+        {/* packing and weight list */}
+        <ContentRoute
+          path="/managementExport/exptransaction/packingnweightlist/edit/:id"
+          component={PackingAndWeightListCreateEdit}
+        />
+        <ContentRoute
+          path="/managementExport/exptransaction/packingnweightlist/create"
+          component={PackingAndWeightListCreateEdit}
+        />
+        <ContentRoute
+          path="/managementExport/exptransaction/packingnweightlist"
+          component={PackingAndWeightListLanding}
+        />
+
+        {/* Job Order */}
+        <ContentRoute
+          path="/managementExport/exptransaction/joborder"
+          component={JobOrderLanding}
+        />
+      </Switch>
     );
 }
 
