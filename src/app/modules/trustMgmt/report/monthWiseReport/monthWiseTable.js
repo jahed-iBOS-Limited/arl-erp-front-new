780a6be425147f73ed326de632bbe6a978436c6c
Obaidul Islam <81848149+theobaidul@users.noreply.github.com>
2023-09-10 17:02:58 +0600

    payment type separation in month wise donation summery report

--- src/app/modules/trustMgmt/report/monthWiseReport/monthWiseTable.js
+++ src/app/modules/trustMgmt/report/monthWiseReport/monthWiseTable.js
@@ -1,27 +1,41 @@
 import React from "react";
-import ICustomTable from "../../../_helper/_customTable";
-
-const header = ["Month & Year", "Cash Pay", "Bank Pay", "Total Amount"];
+import { _formatMoney } from "../../../_helper/_formatMoney";
 
 const MonthWiseTable = ({ rowDto }) => {
   return (
     <>
       <div className="row">
-        <div className="col-lg-8">
+        <div className="col-lg-12">
           <h6 style={{ marginBottom: 0, paddingTop: "30px" }}>Month Wise Donation Summery:</h6>
-          <ICustomTable ths={header}>
-            {
-              rowDto?.length > 0 &&
-              rowDto?.map((item, index) => (
-                <tr className="text-center" key={index}>
-                  <td className={index === rowDto.length - 1 ? "font-weight-bold" : ""}>{item?.strMonthYear}</td>
-                  <td className={index === rowDto.length - 1 ? "font-weight-bold" : ""}>{item?.monCash}</td>
-                  <td className={index === rowDto.length - 1 ? "font-weight-bold" : ""}>{item?.monOnline}</td>
-                  <td className={index === rowDto.length - 1 ? "font-weight-bold" : ""}>{item?.monTotal}</td>
-                </tr>
-              ))
-            }
-          </ICustomTable>
+          <table className="table table-striped table-bordered mt-3 bj-table bj-table-landing">
+            <thead>
+              <tr>
+                <th rowSpan={2}>Month & Year</th>
+                <th colSpan={2}>Zakat</th>
+                <th colSpan={2}>Donation</th>
+                <th rowSpan={2}>Total Amount</th>
+              </tr>
+              <tr>
+                <th>Cash Pay</th>
+                <th>Bank Pay</th>
+                <th>Cash Pay</th>
+                <th>Bank Pay</th>
+              </tr>
+            </thead>
+            <tbody>
+              {rowDto?.length > 0 &&
+                rowDto?.map((item, index) => (
+                  <tr key={index}>
+                    <td className={index === rowDto.length - 1 ? "font-weight-bold" : ""}>{item?.strMonthYear}</td>
+                    <td className={`text-right ${index === rowDto.length - 1 ? "font-weight-bold" : ""}`}>{_formatMoney(item?.monJakatCash || 0)}</td>
+                    <td className={`text-right ${index === rowDto.length - 1 ? "font-weight-bold" : ""}`}>{_formatMoney(item?.monJakatOnline || 0)}</td>
+                    <td className={`text-right ${index === rowDto.length - 1 ? "font-weight-bold" : ""}`}>{_formatMoney(item?.monDonationCash || 0)}</td>
+                    <td className={`text-right ${index === rowDto.length - 1 ? "font-weight-bold" : ""}`}>{_formatMoney(item?.monDonationOnline || 0)}</td>
+                    <td className={`text-right ${index === rowDto.length - 1 ? "font-weight-bold" : ""}`}>{_formatMoney(item?.monTotal || 0)}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </>
