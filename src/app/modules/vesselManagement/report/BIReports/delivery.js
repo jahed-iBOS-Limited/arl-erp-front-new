c54185ea9d555bba9ec61a195e07102689c38975
mahmudhasan <mahmudhasan.akij@gmail.com>
2023-09-25 11:28:48 +0600

    delivery report in G2G

--- src/app/modules/vesselManagement/report/BIReports/delivery.js
+++ src/app/modules/vesselManagement/report/BIReports/delivery.js
@@ -5,14 +5,21 @@ import PowerBIReport from "../../../_helper/commonInputFieldsGroups/PowerBIRepor
 import IButton from "../../../_helper/iButton";
 import ICustomCard from "../../../_helper/_customCard";
 import NewSelect from "../../../_helper/_select";
-import { GetLighterVesselDDL, getMotherVesselDDL, getShippointDDL } from "./helper";
+import {
+  GetLighterVesselDDL,
+  // getMotherVesselDDL,
+  getShippointDDL,
+} from "./helper";
+import FromDateToDateForm from "../../../_helper/commonInputFieldsGroups/dateForm";
+import { _todayDate } from "../../../_helper/_todayDate";
+import { PortAndMotherVessel } from "../../common/components";
 
 const DeliveryReportRDLC = () => {
   const groupId = `e3ce45bb-e65e-43d7-9ad1-4aa4b958b29a`;
   const reportId = `cf508add-c97b-4925-b5ba-3a3d9d170e61`;
 
   const [showReport, setShowReport] = React.useState(false);
-  const [motherVesselDDL, setMotherVesselDDL] = React.useState([]);
+  // const [motherVesselDDL, setMotherVesselDDL] = React.useState([]);
   const [lighterVessel, setLighterVessel] = React.useState([]);
   const [shippointDDL, setShippointDDL] = React.useState([]);
 
@@ -21,14 +28,30 @@ const DeliveryReportRDLC = () => {
     shippoint: "",
     motherVessel: "",
     lighterVessel: "",
+    port: "",
+    soldToPartner: "",
+    fromDate: _todayDate(),
+    toDate: _todayDate(),
   };
 
   const parameterValues = (values) => {
+    console.log(values, "values in params");
+    console.log(values?.port?.value.toString(), "port");
     return [
       { name: "intpartid", value: `${+values?.type?.value}` },
-      { name: "intshippingPointid", value: `${+values?.shippoint?.value}` },
+      {
+        name: "intshippingPointid",
+        value: `${values?.shippoint?.value?.toString()}`,
+      },
       { name: "intMotherVesselId", value: `${+values?.motherVessel?.value}` },
-      { name: "intlighterid", value: `${+values?.lighterVessel?.value}` }
+      { name: "intlighterid", value: `${+values?.lighterVessel?.value}` },
+      { name: "dteFromdDate", value: `${values?.fromDate}` },
+      { name: "dteToDate", value: `${values?.toDate}` },
+      {
+        name: "intSoldToPartnerId",
+        value: `${values?.soldToPartner?.value?.toString()}`,
+      },
+      { name: "IntPortId", value: `${values?.port?.value}` },
     ];
   };
 
@@ -39,7 +62,7 @@ const DeliveryReportRDLC = () => {
 
   useEffect(() => {
     getShippointDDL(accId, buId, setShippointDDL);
-    getMotherVesselDDL(accId, buId, 0, setMotherVesselDDL);
+    // getMotherVesselDDL(accId, buId, 0, setMotherVesselDDL);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [accId, buId]);
 
@@ -48,6 +71,7 @@ const DeliveryReportRDLC = () => {
       <Formik enableReinitialize={true} initialValues={initData}>
         {({ values, setFieldValue }) => (
           <ICustomCard title="Delivery Report">
+            {console.log(values, "values")}
             <form className="form form-label-right">
               <div className="form-group row global-form">
                 <div className="col-lg-3">
@@ -67,13 +91,37 @@ const DeliveryReportRDLC = () => {
                     placeholder="Type"
                   />
                 </div>
+                <FromDateToDateForm
+                  obj={{
+                    values,
+                    setFieldValue,
+                    onChange: () => {
+                      setShowReport(false);
+                    },
+                  }}
+                />
+                <div className="col-lg-3">
+                  <NewSelect
+                    name="soldToPartner"
+                    options={[
+                      { value: 73244, label: "G2G BADC" },
+                      { value: 73245, label: "G2G BCIC" },
+                    ]}
+                    value={values?.soldToPartner}
+                    label="Business Partner"
+                    onChange={(e) => {
+                      setFieldValue("soldToPartner", e);
+                      setShowReport(false);
+                    }}
+                    placeholder="Business Partner"
+                  />
+                </div>
                 <div className="col-lg-3">
                   <NewSelect
                     name="shippoint"
-                    options={[
-                      { value: 0, label: "All" },
-                      ...shippointDDL
-                    ] || []}
+                    options={
+                      [{ value: 0, label: "All" }, ...shippointDDL] || []
+                    }
                     label="Shippoint"
                     value={values?.shippoint}
                     onChange={(valueOption) => {
@@ -83,13 +131,29 @@ const DeliveryReportRDLC = () => {
                     placeholder="Shippoint"
                   />
                 </div>
-                <div className="col-lg-3">
+                <PortAndMotherVessel
+                  obj={{
+                    values,
+                    setFieldValue,
+                    onChange: (fieldName, allValues) => {
+                      setShowReport(false);
+
+                      if (fieldName === "motherVessel") {
+                        GetLighterVesselDDL(
+                          allValues?.motherVessel?.value,
+                          setLighterVessel
+                        );
+                        setFieldValue("lighterVessel", "");
+                      }
+                    },
+                  }}
+                />
+                {/* <div className="col-lg-3">
                   <NewSelect
                     name="motherVessel"
-                    options={[
-                      { value: 0, label: "All" },
-                      ...motherVesselDDL
-                    ] || []}
+                    options={
+                      [{ value: 0, label: "All" }, ...motherVesselDDL] || []
+                    }
                     value={values?.motherVessel}
                     label="Mother Vessel"
                     onChange={(valueOption) => {
@@ -104,14 +168,13 @@ const DeliveryReportRDLC = () => {
                     }}
                     placeholder="Mother Vessel"
                   />
-                </div>
+                </div> */}
                 <div className="col-lg-3">
                   <NewSelect
                     name="lighterVessel"
-                    options={[
-                      { value: 0, label: "All" },
-                      ...lighterVessel
-                    ] || []}
+                    options={
+                      [{ value: 0, label: "All" }, ...lighterVessel] || []
+                    }
                     label="Lighter Vessel"
                     value={values?.lighterVessel}
                     onChange={(valueOption) => {
@@ -122,7 +185,7 @@ const DeliveryReportRDLC = () => {
                   />
                 </div>
                 <IButton
-                  colSize={"col-lg-1"}
+                  // colSize={"col-lg-1"}
                   onClick={() => {
                     setShowReport(false);
                     setShowReport(true);
