76ba690b6bac8b5e8b215beb147486283fdac960
sh-mihad <sh.mihad.cse@gmail.com>
2023-09-25 18:49:58 +0600

    add more three menu landign in BudgetVeriance Report Module

--- src/app/modules/internalControl/budgetVarianceReports/budgetVarianceReportPages.js
+++ src/app/modules/internalControl/budgetVarianceReports/budgetVarianceReportPages.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Redirect, Switch } from "react-router-dom";
 import { ContentRoute } from "../../../../_metronic/layout";
+import CostOfProductionReport from "./costProductionReport";
+import MaterialConsumptionVarianceReport from "./materialConsumptionReport";
+import ProductionVarianceReport from "./productionVarianceReport";
 import BudgetVsSalesVarient from "./salesBudgetVarianceReport";
 
 
@@ -12,9 +15,25 @@ const BudgetVarianceReportPages = () => {
         from="/internal-control/budgetvariancereport"
         to="/internal-control/budgetvariancereport/BudgetVSSalesVariance"
       />
+      {/* <ContentRoute
+        path="/internal-control/budgetvariancereport/BudgetVSSalesVariance/test"
+        component={BudgetVarianceReport}
+      /> */}
       <ContentRoute
         path="/internal-control/budgetvariancereport/BudgetVSSalesVariance"
         component={BudgetVsSalesVarient}
+      />
+      <ContentRoute
+        path="/internal-control/budgetvariancereport/productvariancereport"
+        component={ProductionVarianceReport}
+      />
+      <ContentRoute
+        path="/internal-control/budgetvariancereport/costofproductionreport"
+        component={CostOfProductionReport}
+      />
+      <ContentRoute
+        path="/internal-control/budgetvariancereport/materialconsumptionreport"
+        component={MaterialConsumptionVarianceReport}
       />
       
     </Switch>
