0da3fc2bbead0266dcb2e422f856c7f01eaccd42
jahedhossain <jahed.w.dev@gmail.com>
2023-10-02 12:13:40 +0600

    Sales Report: Sellable Report

--- src/app/modules/salesManagement/orderManagement/salesReportOMS/helper.js
+++ src/app/modules/salesManagement/orderManagement/salesReportOMS/helper.js
@@ -30,6 +30,38 @@ export const getTaxSalesReport_api = async (
   }
 };
 
+export const GetSellableReportApi = async (
+  buId,
+  itemId,
+  itemTypeId,
+  fromDate,
+  toDate,
+  plantId,
+  warehouseId,
+  pageNo,
+  pageSize,
+  setter,
+  setLoading
+) => {
+  try {
+    setLoading(true);
+    const res = await Axios.get(
+      `/procurement/Report/GetSellableReport?businessUnitId=${buId}&fromDate=${fromDate}&toDate=${toDate}&intPlantId=${plantId}&intItemTypeId=${itemTypeId}&itemId=${itemId}&warehouseId=${warehouseId}&pageNo=${pageNo}&pageSize=${pageSize}`
+    );
+    if (res?.data?.length === 0) toast.warning("Data not found");
+    const result = res?.data?.map((itm) => ({
+      ...itm,
+      qty: Math.round(itm?.qty),
+      rate: Math.round(itm?.rate),
+      totalAmount: Math.round(itm?.totalAmount),
+    }));
+    setter(result);
+    setLoading(false);
+  } catch (error) {
+    setLoading(false);
+  }
+};
+
 export const getItemDDL_api = async (accId, buId, setter) => {
   try {
     const res = await Axios.get(
@@ -56,6 +88,35 @@ export const getPartnerNameDDL_api = async (accid, buid, setter) => {
   try {
     const res = await Axios.get(
       `/partner/PManagementCommonDDL/GetBusinessPartnerbyIdDDL?AccountId=${accid}&BusinessUnitId=${buid}&PartnerTypeId=2`
+    );
+    if (res.status === 200 && res?.data) {
+      setter(res?.data);
+    }
+  } catch (error) {}
+};
+
+export const GetItemTypeDDL_api = async (setter) => {
+  try {
+    const res = await Axios.get(`/vat/TaxDDL/GetTaxItemTypeDDL`);
+    if (res.status === 200 && res?.data) {
+      setter(res?.data);
+    }
+  } catch (error) {}
+};
+export const GetItemNameDDL_api = async (accId, buId, typeId, setter) => {
+  try {
+    const res = await Axios.get(
+      `/vat/TaxDDL/GetTaxItemListByItemTypeDDL?AccountId=${accId}&BusinessUnitId=${buId}&TaxItemTypeId=${typeId}`
+    );
+    if (res.status === 200 && res?.data) {
+      setter(res?.data);
+    }
+  } catch (error) {}
+};
+export const getWarehouseDDL = async (userId, accId, buId, plantId, setter) => {
+  try {
+    const res = await Axios.get(
+      `/wms/BusinessUnitPlant/GetOrganizationalUnitUserPermissionforWearhouse?UserId=${userId}&AccId=${accId}&BusinessUnitId=${buId}&PlantId=${plantId}&OrgUnitTypeId=8`
     );
     if (res.status === 200 && res?.data) {
       setter(res?.data);
