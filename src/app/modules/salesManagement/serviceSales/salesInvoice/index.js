72e214e69650b3dac6eb3c2f30188343c11f4cc8
najmus2808 <najmus2808@gmail.com>
2023-10-05 13:07:34 +0600

    design running

--- src/app/modules/salesManagement/serviceSales/salesInvoice/index.js
+++ src/app/modules/salesManagement/serviceSales/salesInvoice/index.js
@@ -6,6 +6,7 @@ import Loading from "./../../../_helper/_loading";
 import NewSelect from "../../../_helper/_select";
 import { shallowEqual, useSelector } from "react-redux";
 import useAxiosGet from "../../../_helper/customHooks/useAxiosGet";
+import { _dateFormatter } from "../../../_helper/_dateFormate";
 const initData = {};
 export default function SalesInvoiceLanding() {
   const { profileData, selectedBusinessUnit } = useSelector((state) => {
@@ -14,6 +15,7 @@ export default function SalesInvoiceLanding() {
 
   const [customerList, getCustomerList] = useAxiosGet();
   const [itemDDL, getItemDDL] = useAxiosGet();
+  const [rowData, getRowData, Loading] = useAxiosGet();
 
   useEffect(() => {
     getCustomerList(
@@ -104,11 +106,89 @@ export default function SalesInvoiceLanding() {
                       className="btn btn-primary"
                       type="button"
                       style={{ marginTop: "17px" }}
+                      // disabled={!values?.customer && !values?.item}
+                      onClick={() => {
+                        getRowData(
+                          `/oms/ServiceSales/GetServiceSalesWithSchedules?accountId=${
+                            profileData?.accountId
+                          }&businessUnitId=${
+                            selectedBusinessUnit?.value
+                          }&customerId=${values?.customer?.value ||
+                            0}&itemId=${values?.item?.value || 0}`
+                        );
+                      }}
                     >
                       Show
                     </button>
                   </div>
                 </div>
+                {rowData?.map((item, i) => (
+                  <div className="mt-5">
+                    <div className="form-group  global-form row">
+                      <table style={{ width: "100%" }}>
+                        <tr>
+                          <td>
+                            Customer:{" "}
+                            <strong>{item?.header?.strCustomerName}</strong>
+                          </td>
+                          <td>
+                            Address:{" "}
+                            <strong>{item?.header?.strCustomerAddress}</strong>
+                          </td>
+                          <td>
+                            Schedule Type:{" "}
+                            <strong>{item?.header?.strScheduleTypeName}</strong>
+                          </td>
+                        </tr>
+                        <tr>
+                        <td>
+                            Sales Type:{" "}
+                            <strong>{item?.header?.strSalesTypeName}</strong>
+                          </td>
+                          <td>
+                            S Service Sales Order Code:{" "}
+                            <strong>
+                              {item?.header?.strServiceSalesOrderCode}
+                            </strong>
+                          </td>
+                          <td></td>
+                        </tr>
+                      </table>
+                    </div>
+                    <div className="">
+                      <div>
+                        <table className="table table-striped table-bordered bj-table bj-table-landing">
+                          <thead>
+                            <tr>
+                              <th>Customer</th>
+                              <th>Schedule Type</th>
+                              <th>Item Name</th>
+                              <th>Due Date</th>
+                              <th>Payment Percent</th>
+                              <th>Schedule Amount</th>
+                              <th>Status</th>
+                              <th>Action</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {item?.scheduleList?.map((item, index) => (
+                              <tr key={index}>
+                                <td>{item?.strCustomerName}</td>
+                                <td>{item?.strScheduleTypeName}</td>
+                                <td>{item?.strItemName}</td>
+                                <td>{_dateFormatter(item?.dteDueDateTime)}</td>
+                                <td>{item?.intPaymentByPercent}</td>
+                                <td>{item?.numScheduleAmount}</td>
+                                <td>{item?.isInvoiceComplete}</td>
+                                <td>{item?.isInvoiceComplete}</td>
+                              </tr>
+                            ))}
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                  </div>
+                ))}
               </div>
             </Form>
           </IForm>
