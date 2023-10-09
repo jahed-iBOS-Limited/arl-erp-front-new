3c61f914ac179a6ae206cf1fd317729bc64f09dd
Obaidul Islam <81848149+theobaidul@users.noreply.github.com>
2023-10-09 15:06:30 +0600

    attachment added in common approval purchase order table

---
+++ src/app/modules/personal/approval/commonApproval/purchaseOrder/attachmentView.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import useAxiosGet from "../../../../_helper/customHooks/useAxiosGet";
+import Loading from "../../../../_helper/_loading";
+import IView from "../../../../_helper/_helperIcons/_view";
+import { getDownlloadFileView_Action } from "../../../../_helper/_redux/Actions";
+
+export default function AttachmentView({ attachmentModalInfo }) {
+  const { purchaseOrderId, orderTypeId } = attachmentModalInfo || {};
+  const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(false);
+  const [rowData, getRowData, rowDataLoader] = useAxiosGet();
+
+  useEffect(() => {
+    getRowData(
+      `/procurement/PurchaseOrder/GetPurchaseOrderInformationByPOtoPrint_Id?PurchaseOrderId=${purchaseOrderId}&OrderTypeId=${orderTypeId}`
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      {(rowDataLoader || isLoading) && <Loading />}
+      <div className="text-right">
+        <table className="table table-striped table-bordered">
+          <thead>
+            <th>SL</th>
+            <th>Attachment</th>
+          </thead>
+          <tbody>
+            {rowData?.[0]?.objAttachmentListDTO?.length > 0 &&
+              rowData?.[0]?.objAttachmentListDTO?.map((item, index) => (
+                <tr key={index}>
+                  <td> {index + 1} </td>
+                  <td className="text-center">
+                    <IView
+                      title={"Attachment"}
+                      classes={"text-primary"}
+                      clickHandler={() => {
+                        dispatch(
+                          getDownlloadFileView_Action(item?.imageId, null, null, setIsLoading)
+                        );
+                      }}
+                    />
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
