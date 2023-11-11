e2ee848edaae98a16c5854b7be76420ae1524d3f
mahmudhasan <mahmudhasan.akij@gmail.com>
2023-11-11 16:04:46 +0600

    adjustment journal for chartering

---
+++ src/app/modules/chartering/reports/incomeReport/journalDetails.js
@@ -0,0 +1,88 @@
+import { Formik } from "formik";
+import React from "react";
+import ICustomCard from "../../../_helper/_customCard";
+import { _fixedPoint } from "../../../_helper/_fixedPoint";
+
+const JournalDetails = ({ obj }) => {
+  const { rowData, buName, buAddress } = obj;
+
+  let totalAmount = 0;
+
+  return (
+    <>
+      <Formik enableReinitialize={true} initialValues={{}} onSubmit={() => {}}>
+        {() => (
+          <>
+            <ICustomCard title="Journal Details">
+              <form className="form form-label-right">
+                <div id="pdf-section">
+                  <div className="text-center">
+                    <h2>{buName.toUpperCase()}</h2>
+                    <h6
+                      style={{
+                        borderBottom: "2px solid #ccc",
+                        paddingBottom: "10px",
+                        fontSize: "12px",
+                        fontWeight: "bold",
+                      }}
+                    >
+                      {buAddress}
+                    </h6>
+                    {/* <h3 className="m-0">{tableItem?.strSubGlName}</h3> */}
+                  </div>
+
+                  <div className="react-bootstrap-table table-responsive">
+                    <table
+                      className="table table-striped table-bordered global-table"
+                      id="table-to-xls"
+                    >
+                      <thead>
+                        <tr>
+                          <th style={{ width: "30px" }}> SL </th>
+                          <th> Code </th>
+                          <th> Gl Name </th>
+                          <th>Business Transaction</th>
+                          <th> Cost Center </th>
+                          <th> Gl Narration </th>
+                          <th> Amount </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {rowData?.map((item, index) => {
+                          totalAmount += item?.numAmount;
+                          return (
+                            <tr key={index}>
+                              <td className="text-center">{index + 1}</td>
+                              <td>{item?.strAccountingJournalCode}</td>
+                              <td>{item?.strGeneralLedgerName}</td>
+                              <td>{item?.strSubGlname}</td>
+                              <td>{item?.strCostRevenueName}</td>
+                              <td>{item?.strNarration}</td>
+                              <td className="text-right">
+                                {_fixedPoint(item?.numAmount, true)}
+                              </td>
+                            </tr>
+                          );
+                        })}
+                        <tr>
+                          <td className="text-right" colSpan="6">
+                            <b>Total</b>
+                          </td>
+                          <td className="text-right">
+                            <b> {_fixedPoint(totalAmount, true)}</b>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </form>
+            </ICustomCard>
+          </>
+        )}
+      </Formik>
+    </>
+  );
+};
+
+export default JournalDetails;
