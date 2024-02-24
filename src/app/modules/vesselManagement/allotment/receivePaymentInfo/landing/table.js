39ce848bcab8f192dc63d53b3724edb94f7a5919
mahmudhasan <mahmudhasan.akij@gmail.com>
2024-02-24 07:42:41 +0600

    receive payment info done

--- src/app/modules/vesselManagement/allotment/receivePaymentInfo/landing/table.js
+++ src/app/modules/vesselManagement/allotment/receivePaymentInfo/landing/table.js
@@ -1,12 +1,23 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React from "react";
 import { _dateFormatter } from "../../../../_helper/_dateFormate";
-import { _fixedPoint } from "../../../../_helper/_fixedPoint";
 
-const headers = ["SL", "Date", "Item Name", "Warehouse", "Quantity", "Action"];
+const headers = [
+  "SL",
+  "Program No",
+  "Mother Vessel",
+  "Buffer",
+  "Delivery Code",
+  "Delivery Date",
+  "Item",
+  "Quantity (bag)",
+  "Quantity (ton)",
+  "Status",
+  "JV Code",
+];
 
 const ReceivePaymentInfoLandingTable = ({ obj }) => {
-  const { gridData } = obj;
+  const { gridData, values, setGridData } = obj;
 
   return (
     <>
@@ -19,6 +30,28 @@ const ReceivePaymentInfoLandingTable = ({ obj }) => {
         >
           <thead>
             <tr className="cursor-pointer">
+              {[0, 1, 2].includes(values?.status?.value) && (
+                <th>
+                  <input
+                    type="checkbox"
+                    checked={
+                      gridData?.length > 0
+                        ? gridData?.every((item) => item?.isSelected)
+                        : false
+                    }
+                    onChange={(e) => {
+                      setGridData(
+                        gridData?.map((item) => {
+                          return {
+                            ...item,
+                            isSelected: e?.target?.checked,
+                          };
+                        })
+                      );
+                    }}
+                  />
+                </th>
+              )}
               {headers?.map((th, index) => {
                 return <th key={index}> {th} </th>;
               })}
@@ -28,23 +61,32 @@ const ReceivePaymentInfoLandingTable = ({ obj }) => {
             {gridData?.map((item, index) => {
               return (
                 <tr key={index}>
-                  <td> {item?.sl}</td>
-                  <td>{_dateFormatter(item?.transactionDate)}</td>
-                  <td>{item?.itemName}</td>
-                  <td>{item?.warehouseName}</td>
-                  <td className="text-right">
-                    {_fixedPoint(item?.transactionQuantity, true)}
-                  </td>
+                  {" "}
+                  {[0, 1, 2].includes(values?.status?.value) && (
+                    <td className="text-center align-middle">
+                      <input
+                        type="checkbox"
+                        checked={item?.isSelected}
+                        onChange={(e) => {
+                          item["isSelected"] = e.target.checked;
+                          setGridData([...gridData]);
+                        }}
+                      />
+                    </td>
+                  )}
+                  <td> {item?.SL}</td>
+                  <td> {item?.ProgramNo}</td>
+                  <td>{item?.MotherVesselName}</td>
+                  <td>{item?.ShipToPartnerName}</td>
+                  <td> {item?.DeliveryCode}</td>
+                  <td>{_dateFormatter(item?.DeliveryDate)}</td>
+                  <td>{item?.ItemName}</td>
+                  <td className="text-right">{item?.QuantityBag}</td>
+                  <td className="text-right">{item?.QuantityTon}</td>
                   <td>
-                    <div className="d-flex justify-content-around">
-                      {/* <span className="text-center">
-                        <IView clickHandler={() => {}} />
-                      </span>
-                      <span className="edit" onClick={() => {}}>
-                        <IEdit title={"Rate Entry"} />
-                      </span> */}
-                    </div>
+                    {item?.PaymentReceiveStatusName || values?.status?.label}
                   </td>
+                  <td>{item?.JvCode}</td>
                 </tr>
               );
             })}
