f850ff14f7c337624b09b7219cac1456dee2f147
mahmudhasan <mahmudhasan.akij@gmail.com>
2024-02-28 16:23:39 +0600

    dam delivery bill details view

--- src/app/modules/financialManagement/invoiceManagementSystem/billregister/damDelivery/view/table.js
+++ src/app/modules/financialManagement/invoiceManagementSystem/billregister/damDelivery/view/table.js
@@ -8,25 +8,11 @@ import ICard from "../../../../../_helper/_card";
 import Loading from "../../../../../_helper/_loading";
 import { getDownlloadFileView_Action } from "../../../../../_helper/_redux/Actions";
 import useAxiosGet from "../../../../../_helper/customHooks/useAxiosGet";
-import { common_api_for_4_types_of_bill } from "../../helper";
-import BillApproveForm from "../../../approvebillregister/approveForm";
 
-const initData = {
-  approveAmount: "",
-  approveAmountMax: "",
-  remarks: "",
-};
-
-function ViewDamDeliveryBill({
-  billRegisterId,
-  gridItem,
-  landingValues,
-  setModalShow,
-  gridDataFunc,
-}) {
-  // get profile data from store
+function ViewDamDeliveryBill({ billRegisterId }) {
   const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
+  // get profile data from store
   const {
     profileData: { accountId: accId },
     selectedBusinessUnit: { value: buId },
@@ -37,48 +23,32 @@ function ViewDamDeliveryBill({
   const [gridData, getGridData, loadingGridData] = useAxiosGet();
 
   useEffect(() => {
-    const url = common_api_for_4_types_of_bill(accId, buId, billRegisterId, 25);
-    getGridData(url);
+    // const url = `/tms/LigterLoadUnload/GetLighterDumpToTruckDeliveryDetaills?lighterVesselId=2012&shipPointId=272&FromDate=2023-12-01&ToDate=2023-12-31&isDumpToTruckApprove=0`;
+    getGridData("");
   }, [accId, buId]);
 
   return (
     <>
       <Formik
         enableReinitialize={true}
-        initialValues={{
-          ...initData,
-          approveAmount: gridItem?.monTotalAmount,
-          approveAmountMax: gridItem?.monTotalAmount,
-        }}
+        initialValues={{}}
         onSubmit={(values) => {}}
       >
-        {({ values }) => (
-          <ICard title={`View CNF Bill`}>
+        {() => (
+          <ICard title={`View Dam Delivery Bill`}>
             {(loadingGridData || loading) && <Loading />}
 
             <form className="form form-label-right ">
-              <BillApproveForm
-                obj={{
-                  values,
-                  gridItem,
-                  gridDataFunc,
-                  landingValues,
-                  setModalShow,
-                }}
-              />
               <table className="table global-table">
                 <thead>
                   <tr>
                     <th>SL</th>
                     <th>Mother Vessel</th>
                     <th>Lighter Vessel</th>
-                    <th>CNF Name</th>
+                    <th>Hatch Labor Supplier Name</th>
                     <th>Port Name</th>
                     <th>Program Qty</th>
-                    <th>CNF Rate</th>
-                    <th>LC Rate</th>
-                    <th>VAT on CNF</th>
-                    <th>Income TAX on CNF</th>
+                    <th>Hatch Labor Rate</th>
                     <th>Attachment</th>
                   </tr>
                 </thead>
@@ -92,13 +62,12 @@ function ViewDamDeliveryBill({
                           </td>
                           <td>{item?.motherVesselName}</td>
                           <td>{item?.lighterVesselName}</td>
-                          <td>{item?.cnfname}</td>
+                          <td>{item?.hatchLabour}</td>
                           <td>{item?.portName}</td>
                           <td className="text-right">{item?.programQnt}</td>
-                          <td className="text-right">{item?.cnfrate || 0}</td>
-                          <td className="text-right">{item?.lcrate}</td>
-                          <td className="text-right">{item?.vatonCnf}</td>
-                          <td className="text-right">{item?.incomeTaxonCnf}</td>
+                          <td className="text-right">
+                            {item?.hatchLabourRate || 0}
+                          </td>
                           <td className="text-center">
                             <OverlayTrigger
                               overlay={
