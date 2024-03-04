841fd0070a58fc187b7eea8fdde7c4714067c3f8
najmus2808 <najmus2808@gmail.com>
2024-03-04 18:28:07 +0600

    map show done

--- src/app/modules/transportManagement/stuffTransport/transportExpenseReport/reportTable/driverTripInfoTbl.js
+++ src/app/modules/transportManagement/stuffTransport/transportExpenseReport/reportTable/driverTripInfoTbl.js
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { _dateFormatter } from "../../../../_helper/_dateFormate";
 import { _formatMoney } from "../../../../_helper/_formatMoney";
+import IView from "../../../../_helper/_helperIcons/_view";
+import { useHistory } from "react-router";
+import IViewModal from "../../../../_helper/_viewModal";
+import MapView from "../mapView";
 
 export default function DriverTripInfoTbl({ rowData }) {
+  const [isShowModal, setIsShowModal] = useState(false);
+  const [singleData, setSingleData] = useState({})
   const totalKM = rowData?.reduce((acc, curr) => acc + curr?.tripKM, 0);
-  const totalTollAmount = rowData?.reduce((acc, curr) => acc + curr?.numTollAmount, 0);
-  const totalFoodAmount = rowData?.reduce((acc, curr) => acc + curr?.numFoodAmount, 0);
-  const totalOthers = rowData?.reduce((acc, curr) => acc + curr?.numOthersAmount, 0)
-  const totalMaintenance = rowData?.reduce((acc, curr) => acc + curr?.numRepairingAmount, 0)
+  const totalTollAmount = rowData?.reduce(
+    (acc, curr) => acc + curr?.numTollAmount,
+    0
+  );
+  const totalFoodAmount = rowData?.reduce(
+    (acc, curr) => acc + curr?.numFoodAmount,
+    0
+  );
+  const totalOthers = rowData?.reduce(
+    (acc, curr) => acc + curr?.numOthersAmount,
+    0
+  );
+  const totalMaintenance = rowData?.reduce(
+    (acc, curr) => acc + curr?.numRepairingAmount,
+    0
+  );
   const totalDriverCost = rowData?.reduce((acc, curr) => {
-    const total = curr?.numTollAmount + curr?.numOthersAmount + curr?.numRepairingAmount
+    const total =
+      curr?.numTollAmount + curr?.numOthersAmount + curr?.numRepairingAmount;
     return acc + total;
   }, 0);
 
   const totalLPG = rowData?.reduce((acc, curr) => acc + curr?.lpeg, 0);
-  const totalDiesel = rowData?.reduce((acc, curr)=> acc = curr?.diesel, 0);
-  const totalOctane = rowData?.reduce((acc, curr)=> acc = curr?.octane, 0);
+  const totalDiesel = rowData?.reduce((acc, curr) => (acc = curr?.diesel), 0);
+  const totalOctane = rowData?.reduce((acc, curr) => (acc = curr?.octane), 0);
   const totalFuelCost = totalLPG + totalDiesel + totalOctane;
   return (
     <div>
@@ -41,40 +60,52 @@ export default function DriverTripInfoTbl({ rowData }) {
             <th>Diesel</th>
             <th>Octane </th>
             <th>Total Fuel Cost</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>
-          {rowData?.length > 0 && rowData?.map((item, index) => (
-            <tr key={index}>
-              <td className="text-center">{index + 1}</td>
-              <td className="text-center">
-                {_dateFormatter(item?.dteTripDate)}
-              </td>
-              <td></td>
-              <td className="text-center">{item?.tripKM}</td>
-              <td className="text-center">{item?.strVehicleNo}</td>
-              <td>{item?.strFirstRoundStartAddress}</td>
-              <td>{item?.strFirstRoundEndAddress}</td>
-              <td className="text-right">{item?.numTollAmount}</td>
-              <td className="text-right">{item?.numFoodAmount}</td>
-              <td className="text-right">{item?.numOthersAmount}</td>
-              <td className="text-right">{item?.numRepairingAmount}</td>
-              <td className="text-right">
-                {item?.numTollAmount +
-                  item?.numOthersAmount +
-                  item?.numRepairingAmount}
-              </td>
-              <td className="text-right">{item?.lpeg}</td>
-              <td className="text-right">{item?.diesel}</td>
-              <td className="text-right">{item?.octane}</td>
-              <td className="text-right">
-                {item?.lpeg + item?.diesel + item?.octane}
-              </td>
-            </tr>
-          ))}
-          {rowData?.length > 0 && 
-            <tr style={{fontWeight: "bold"}}>
-              <td colSpan={3} >Total</td>
+          {rowData?.length > 0 &&
+            rowData?.map((item, index) => (
+              <tr key={index}>
+                <td className="text-center">{index + 1}</td>
+                <td className="text-center">
+                  {_dateFormatter(item?.dteTripDate)}
+                </td>
+                <td></td>
+                <td className="text-center">{item?.tripKM}</td>
+                <td className="text-center">{item?.strVehicleNo}</td>
+                <td>{item?.strFirstRoundStartAddress}</td>
+                <td>{item?.strFirstRoundEndAddress}</td>
+                <td className="text-right">{item?.numTollAmount}</td>
+                <td className="text-right">{item?.numFoodAmount}</td>
+                <td className="text-right">{item?.numOthersAmount}</td>
+                <td className="text-right">{item?.numRepairingAmount}</td>
+                <td className="text-right">
+                  {item?.numTollAmount +
+                    item?.numOthersAmount +
+                    item?.numRepairingAmount}
+                </td>
+                <td className="text-right">{item?.lpeg}</td>
+                <td className="text-right">{item?.diesel}</td>
+                <td className="text-right">{item?.octane}</td>
+                <td className="text-right">
+                  {item?.lpeg + item?.diesel + item?.octane}
+                </td>
+                <td className="text-center">
+                  <span
+                    onClick={() => {
+                      setSingleData(item);
+                      setIsShowModal(true);
+                    }}
+                  >
+                    <IView />
+                  </span>
+                </td>
+              </tr>
+            ))}
+          {rowData?.length > 0 && (
+            <tr style={{ fontWeight: "bold" }}>
+              <td colSpan={3}>Total</td>
               <td className="text-right"> {totalKM} </td>
               <td colSpan={3}></td>
               <td className="text-right">{_formatMoney(totalTollAmount)}</td>
@@ -87,9 +118,20 @@ export default function DriverTripInfoTbl({ rowData }) {
               <td className="text-right">{_formatMoney(totalOctane)}</td>
               <td className="text-right">{_formatMoney(totalFuelCost)}</td>
             </tr>
-          }
+          )}
         </tbody>
       </table>
+      <div>
+        <IViewModal
+          show={isShowModal}
+          onHide={() => {
+            setIsShowModal(false);
+          }}
+          title="Trip Details Info Report"
+        >
+          <MapView singleData={singleData} />
+        </IViewModal>
+      </div>
     </div>
   );
 }
