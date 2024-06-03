15cc8ded4a654698ceb75b20073cdfa3f8abd360
jahedhossin <jahed.w.dev@gmail.com>
2024-06-03 14:25:22 +0600

    fix issue

--- src/app/modules/salesManagement/complainManagement/resolution/landing/index.js
+++ src/app/modules/salesManagement/complainManagement/resolution/landing/index.js
@@ -239,6 +239,7 @@ const ResolutionLanding = () => {
                     commonGridData(pageNo, pageSize, values);
                   },
                   setLoading,
+                  title
                 }}
               />
 
