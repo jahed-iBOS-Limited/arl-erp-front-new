0b6c2e4d91638ba4ab3b942550ade875659cc99b
mdsifat <ibnsifat@gmail.com>
2024-06-05 21:08:49 +0600

    update

--- src/app/modules/financialManagement/report/bankLetter/printDocuments/templates/Authorization/one.js
+++ src/app/modules/financialManagement/report/bankLetter/printDocuments/templates/Authorization/one.js
@@ -39,128 +39,54 @@ const AuthorizationOne = ({ singleRowItem }) => {
         <p style={{}}>
           <b>
             {" "}
-            REF : {"{"}
-            {"{"}
-            {strBusinessUnitShortName}
-            {"}"}
-            {"}"}/{"{"}
-            {"{"}
-            {strBankName}
-            {"}"}
-            {"}"}/Doc/{"{"}
-            {"{"}
+            REF :{strBusinessUnitShortName}/{strBankName}
+            /Doc/
             {strRefDate}
-            {"}"}
-            {"}"}
           </b>
         </p>
         <p style={{ marginTop: "-15px" }}>
-          <b>
-            Date : {"{"}
-            {"{"}
-            {strDate}
-            {"}"}
-            {"}"}
-          </b>
+          <b>Date :{strDate}</b>
         </p>
         <p style={{ marginTop: 35 }}>To</p>
         <p style={{ marginTop: "-15px" }}>The Head of the Branch</p>
+        <p style={{ marginTop: "-15px" }}>{strBankName}</p>
         <p style={{ marginTop: "-15px" }}>
-          {"{"}
-          {"{"}
-          {strBankName}
-          {"}"}
-          {"}"}
-        </p>
-        <p style={{ marginTop: "-15px" }}>
-          {"{"}
-          {"{"}
           {strBranchName}
-          {"}"}
-          {"}"} Branch
+          Branch
         </p>
-        <p style={{ marginTop: "-15px" }}>
-          {"{"}
-          {"{"}
-          {strBranchAddress}
-          {"}"}
-          {"}"}
-        </p>
+        <p style={{ marginTop: "-15px" }}>{strBranchAddress}</p>
         <p style={{ margin: "35px 0" }}>
           <b>
-            Subject: Authorization to receive {"{"}
-            {"{"}
+            Subject: Authorization to receive
             {strDocumentName}
-            {"}"}
-            {"}"} of {"{"}
-            {"{"}
+            of
             {strBusinessUnitName}
-            {"}"}
-            {"}"}
             {strAccountType}
-            {"}"}
-            {"}"} A/c No- {"{"}
-            {"{"}
-            {strAccountNo}
-            {"}"}
-            {"}"}.
+            A/c No-
+            {strAccountNo}.
           </b>
         </p>
         <p style={{ marginTop: 35 }}>Dear Sir/ Madam,</p>
         <p>As-salamu alaykum</p>
         <p style={{ marginTop: "-10px" }}>
-          We do hereby authorize {"{"}
-          {"{"}
-          {strMassengerName}
-          {"}"}
-          {"}"}, {"{"}
-          {"{"}
-          {strMessengerDesignation}
-          {"}"}
-          {"}"}of our company to receive {"{"}
-          {"{"}
+          We do hereby authorize
+          {strMassengerName},{strMessengerDesignation}
+          of our company to receive
           {strDocumentName}
-          {"}"}
-          {"}"} of {"{"}
-          {"{"}
-          {strBusinessUnitName}
-          {"}"}
-          {"}"}, A/C No-
-          {"{"}
-          {"{"}
-          {strAccountNo}
-          {"}"}
-          {"}"}. His specimen signature is attested below.
+          of
+          {strBusinessUnitName}, A/C No-
+          {strAccountNo}. His specimen signature is attested below.
         </p>
         <p style={{ marginTop: 35 }}>
           <b>The Specimen Signature of</b>
         </p>
         <p style={{ marginTop: 35 }}>.....................................</p>
-        <p style={{ marginTop: "-15px" }}>
-          {"{"}
-          {"{"}
-          {strMassengerName}
-          {"}"}
-          {"}"}
-        </p>
-        <p style={{ marginTop: "-15px" }}>
-          {"{"}
-          {"{"}
-          {strMessengerDesignation}
-          {"}"}
-          {"}"}
-        </p>
+        <p style={{ marginTop: "-15px" }}>{strMassengerName}</p>
+        <p style={{ marginTop: "-15px" }}>{strMessengerDesignation}</p>
         <br />
         <p style={{ marginTop: 5 }}>Yours faithfully,</p>
         <p style={{ marginTop: 5 }}>
-          For,{" "}
-          <b>
-            {"{"}
-            {"{"}
-            {strBusinessUnitName}
-            {"}"}
-            {"}"}
-          </b>
+          For, <b>{strBusinessUnitName}</b>
         </p>
         <div style={{ marginTop: 65, display: "flex" }}>
           <div>Authorized Signature</div>
