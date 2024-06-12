cd964b7e2d892eb407146e2cc8f87152dfe21f20
jahedhossin <jahed.w.dev@gmail.com>
2024-06-12 11:35:33 +0600

    fix issue

--- src/app/modules/productionManagement/report/healthSummary/healthSummaryModal.js
+++ src/app/modules/productionManagement/report/healthSummary/healthSummaryModal.js
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from "react";
 import useAxiosGet from "../../../_helper/customHooks/useAxiosGet";
 import { shallowEqual, useSelector } from "react-redux";
+import IViewModal from "../../../_helper/_viewModal";
 function HealthSummaryModal({ clickRowData }) {
+  const [isShowModal, setIsShowModal] = useState(false);
+  const [clickRowDataMachienDetils, setClickRowDataMachienDetils] = useState(
+    {}
+  );
   const { selectedBusinessUnit } = useSelector((state) => {
     return state.authData;
   }, shallowEqual);
@@ -9,14 +14,13 @@ function HealthSummaryModal({ clickRowData }) {
   const [, getHealthCheckPendingList, ,] = useAxiosGet();
   useEffect(() => {
     getHealthCheckPendingList(
-      `/asset/AssetMaintanance/GetMachineHealthCheckPendingList?checkDate=${clickRowData?.fromDate}&businessUnitId=${selectedBusinessUnit?.value}&plantId=${clickRowData?.plant?.value}&shopfloorId=${clickRowData?.shopfloor?.value}&sectionName=${clickRowData?.strSectionName}`,
+      `/asset/AssetMaintanance/GetMachineHealthCheckPendingList?checkDate=${clickRowData?.fromDate}&businessUnitId=${selectedBusinessUnit?.value}&plantId=${clickRowData?.plant?.value}&shopfloorId=${clickRowData?.shopfloor?.value}&sectionName=${clickRowData?.strSectionName}&type=MachineWiseCheckPercentage`,
       (resData) => {
         setGridData(resData);
       }
     );
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
   return (
     <div>
       <div className="table-responsive">
@@ -26,11 +30,13 @@ function HealthSummaryModal({ clickRowData }) {
               <th>SL</th>
               <th>Machine Name</th>
               <th>Section Name</th>
-              <th>CheckList Criteria</th>
-              <th>CheckList Criteria Type</th>
-              <th>HealthCheck Status</th>
-              <th>Work Status</th>
-              <th>Work Section</th>
+              <th>CheckList Criteria Total</th>
+              <th>CheckList Criteria Pending</th>
+              <th>CheckList Criteria Complete</th>
+              <th>Complete(%)</th>
+              <th>Pending(%)</th>
+              <th>CheckList Criteria Status</th>
+              <th>Action</th>
             </tr>
           </thead>
           <tbody>
@@ -39,18 +45,103 @@ function HealthSummaryModal({ clickRowData }) {
                 <td>{index + 1}</td>
                 <td>{item?.machineName}</td>
                 <td>{item?.sectionName}</td>
-                <td>{item?.checkListCriteria}</td>
-                <td>{item?.checkListCriteriaType}</td>
-                <td>{item?.healthCheckStatus}</td>
-                <td>{item?.workStatus}</td>
-                <td>{item?.workSection}</td>
+                <td>{item?.totalCheckListCriteria}</td>
+                <td>{item?.pendingCheckListCriteria}</td>
+                <td>{item?.completeCheckListCriteria}</td>
+                <td>{item?.completePercentage || 0}%</td>
+                <td>{item?.pendingPercentage || 0}&</td>
+                <td>{item?.checkListCriteriaStatus}</td>
+                <td>
+                  <div className="d-flex justify-content-around">
+                    <span
+                      className="btn btn-primary"
+                      onClick={() => {
+                        setIsShowModal(true);
+                        setClickRowDataMachienDetils(item);
+                      }}
+                    >
+                      View
+                    </span>
+                  </div>
+                </td>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
+
+      {isShowModal && (
+        <>
+          <IViewModal
+            show={isShowModal}
+            onHide={() => {
+              setIsShowModal(false);
+              setClickRowDataMachienDetils({});
+            }}
+            title={clickRowDataMachienDetils?.machineName || ""}
+          >
+            <MachienDetils
+              clickRowData={clickRowData}
+              selectedBusinessUnit={selectedBusinessUnit}
+              clickRowDataMachienDetils={clickRowDataMachienDetils}
+            />
+          </IViewModal>
+        </>
+      )}
     </div>
   );
 }
 
 export default HealthSummaryModal;
+
+const MachienDetils = ({
+  clickRowData,
+  selectedBusinessUnit,
+  clickRowDataMachienDetils,
+}) => {
+  const [gridData, setGridData] = useState([]);
+  const [, getHealthCheckPendingList, ,] = useAxiosGet();
+
+  useEffect(() => {
+    getHealthCheckPendingList(
+      `/asset/AssetMaintanance/GetMachineHealthCheckPendingList?checkDate=${clickRowData?.fromDate}&businessUnitId=${selectedBusinessUnit?.value}&plantId=${clickRowData?.plant?.value}&shopfloorId=${clickRowData?.shopfloor?.value}&sectionName=${clickRowData?.strSectionName}&type=MachineWisePendingCheckList&machineId=${clickRowDataMachienDetils?.machineId}`,
+      (resData) => {
+        setGridData(resData);
+      }
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      <div className="table-responsive">
+        <table className="table table-striped table-bordered global-table mt-0">
+          <thead>
+            <tr>
+              <th>SL</th>
+              <th>Section Name</th>
+              <th>CheckList Criteria Type</th>
+              <th>CheckList Criteria</th>
+              <th>Work Section</th>
+              <th>Health Check Status</th>
+              <th>Work Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {gridData?.map((item, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{item?.sectionName}</td>
+                <td>{item?.checkListCriteriaType}</td>
+                <td>{item?.checkListCriteria}</td>
+                <td>{item?.workSection}</td>
+                <td>{item?.healthCheckStatus}</td>
+                <td>{item?.workStatus}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
