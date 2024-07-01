9a93d31e0224f28bafc7495e753ad47065367409
jahedhossin <jahed.w.dev@gmail.com>
2024-07-01 14:44:12 +0600

    fix issue

--- src/app/modules/vesselManagement/allotment/g2gSalesInvoice/index.js
+++ src/app/modules/vesselManagement/allotment/g2gSalesInvoice/index.js
@@ -4,14 +4,13 @@ import { shallowEqual, useSelector } from "react-redux";
 import ReactToPrint from "react-to-print";
 import * as Yup from "yup";
 import ICustomCard from "../../../_helper/_customCard";
+import InputField from "../../../_helper/_inputField";
 import Loading from "../../../_helper/_loading";
 import NewSelect from "../../../_helper/_select";
+import { _todayDate } from "../../../_helper/_todayDate";
 import useAxiosGet from "../../../_helper/customHooks/useAxiosGet";
 import { PortAndMotherVessel } from "../../common/components";
-import InputField from "../../../_helper/_inputField";
-import { _todayDate } from "../../../_helper/_todayDate";
-import { _dateFormatter } from "../../../_helper/_dateFormate";
-import { _fixedPoint } from "../../../_helper/_fixedPoint";
+import GhatWiseDeliveryReport from "./ghatWiseDeliveryReport";
 const validationSchema = Yup.object().shape({});
 function G2GSalesInvoice() {
   const {
@@ -268,97 +267,4 @@ function G2GSalesInvoice() {
 
 export default G2GSalesInvoice;
 
-const GhatWiseDeliveryReport = ({ printRef, gridData, buUnName, values }) => {
-  return (
-    <div ref={printRef}>
-      <div>
-        <div className="row">
-          <div className="col-lg-12 text-center">
-            <h4 className="m-0">{buUnName}</h4>
-            <p>
-              <span>Ghat Wise Delivery Report</span>
-              <br />
-              <span>{values?.motherVessel?.label}</span>
-              <br />
-              <span>{values?.item?.label}</span>
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-lg-12">
-          <div className="scroll-table-auto asset_list">
-            <table className="table table-striped table-bordered global-table table-font-size-sm">
-              <thead>
-                <tr>
-                  <th>SL</th>
-                  <th>Date</th>
-                  <th>Truck No</th>
-                  <th>Challan No</th>
-                  <th>Quantity(MT)</th>
-                  <th>Quantity(BAG)</th>
-                  <th>Epmty Bag</th>
-                  <th>Destination</th>
-                  <th>Receiving Date</th>
-                </tr>
-              </thead>
 
-              <tbody>
-                {gridData?.map((item, index) => (
-                  <tr key={index}>
-                    <td>{index + 1}</td>
-                    <td>{_dateFormatter(item?.deliveryDate)}</td>
-                    <td>{item?.vehicleRegNo}</td>
-                    <td>{item?.deliveryCode}</td>
-                    <td className="text-right">
-                      {item?.totalDeliveryQuantity}
-                    </td>
-                    <td className="text-right">{item?.totalDeliveryValue}</td>
-                    <td className="text-right">{item?.emptyBag}</td>
-                    <td>{item?.shipPointName}</td>
-                    <td>
-                      {item?.receiveDate
-                        ? _dateFormatter(item?.receiveDate)
-                        : ""}
-                    </td>
-                  </tr>
-                ))}
-                <tr>
-                  <td colSpan={4}>Total</td>
-                  <td className="text-right">
-                    <b>
-                      {_fixedPoint(
-                        gridData?.reduce((acc, cur) => {
-                          return (acc += +cur?.totalDeliveryQuantity || 0);
-                        }, 0)
-                      )}
-                    </b>
-                  </td>
-                  <td className="text-right">
-                    <b>
-                      {_fixedPoint(
-                        gridData?.reduce((acc, cur) => {
-                          return (acc += +cur?.totalDeliveryValue || 0);
-                        }, 0)
-                      )}
-                    </b>
-                  </td>
-                  <td className="text-right">
-                    <b>
-                      {_fixedPoint(
-                        gridData?.reduce((acc, cur) => {
-                          return (acc += +cur?.emptyBag || 0);
-                        }, 0)
-                      )}
-                    </b>
-                  </td>
-                  <td colSpan={2}></td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
