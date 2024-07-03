5fa45cf88a93a1cad3f9b402f8e3aabc255bcd36
jahedhossin <jahed.w.dev@gmail.com>
2024-07-03 11:46:48 +0600

    Bank Advice for Banking: date issue fix

--- src/app/modules/financialManagement/banking/bankAdvce/pdf/format-02.js
+++ src/app/modules/financialManagement/banking/bankAdvce/pdf/format-02.js
@@ -52,7 +52,7 @@ const FormatTwo = ({
               To
             </p>
             <p style={{ fontSize: "10px" }} className="font-weight-bold">
-              Date: {dateFormatWithMonthName(_todayDate())}
+              Date: {dateFormatWithMonthName(values?.dateTime)}
             </p>
           </div>
           <p style={{ fontSize: "10px" }} className="font-weight-bold">
