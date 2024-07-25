131ca5b37711fa104454c01ae54f0a736b1ccef7
rifat <rakibul.rifat@ibos.io>
2024-07-25 15:14:06 +0600

    build: landing page of tender submission

---
+++ src/app/modules/vesselManagement/allotment/tenderSubmission/landing/index.js
@@ -0,0 +1,68 @@
+
+import { Form, Formik } from "formik";
+import React from "react";
+import { useHistory } from "react-router-dom";
+import Loading from "../../../../_helper/_loading";
+import IForm from "../../../../_helper/_form";
+
+
+
+// const initData = {};
+
+
+export default function TenderSubmissionLanding() {
+    const saveHandler = (values, cb) => { };
+    const history = useHistory();
+
+    return (
+        <Formik
+            enableReinitialize={true}
+            initialValues={{}}
+            // validationSchema={{}}
+            onSubmit={(values, { setSubmitting, resetForm }) => {
+                saveHandler(values, () => {
+                    resetForm();
+                });
+            }}
+        >
+            {({
+                handleSubmit,
+                resetForm,
+                values,
+                setFieldValue,
+                isValid,
+                errors,
+                touched,
+            }) => (
+                <>
+                    {false && <Loading />}
+                    <IForm
+                        title="Tender Submission"
+                        isHiddenReset
+                        isHiddenBack
+                        isHiddenSave
+                        renderProps={() => {
+                            return (
+                                <div>
+                                    <button
+                                        type="button"
+                                        className="btn btn-primary"
+                                        onClick={() => {
+                                            history.push("/vessel-management/allotment/tendersubmition/entry");
+                                        }}
+                                    >
+                                        Create
+                                    </button>
+                                </div>
+                            );
+                        }}
+                    >
+                        <Form>
+                            <div>Landing here...</div>
+                        </Form>
+                    </IForm>
+                </>
+            )}
+        </Formik>
+    );
+}
