5671df3896c28ffde6981b3580c360f8301f9f6d
mahmudhasan <mahmudhasan.akij@gmail.com>
2024-07-31 10:33:40 +0600

    RAT auto set is done for IHB

--- src/app/modules/config/partner-management/homeBuildersInfo/form/addEditForm.js
+++ src/app/modules/config/partner-management/homeBuildersInfo/form/addEditForm.js
@@ -1,17 +1,18 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { shallowEqual, useSelector } from "react-redux";
 import { useParams } from "react-router";
 import Loading from "../../../../_helper/_loading";
 import useAxiosPost from "../../../../_helper/customHooks/useAxiosPost";
 import Form from "./form";
+import useAxiosGet from "../../../../_helper/customHooks/useAxiosGet";
 
 const initData = {
   channel: "",
   region: "",
   area: "",
   territory: "",
-  type: "",
+  type: { value: 3, label: "IHB" },
   name: "",
   address: "",
   contactNumber: "",
@@ -28,7 +29,7 @@ const initData = {
 export default function HomeBuildersInfoEntryForm() {
   // get user data from store
   const {
-    profileData: { accountId: accId, userId },
+    profileData: { accountId: accId, userId, employeeId },
     selectedBusinessUnit: { value: buId },
   } = useSelector((state) => state?.authData, shallowEqual);
 
@@ -36,6 +37,13 @@ export default function HomeBuildersInfoEntryForm() {
   const [objProps] = useState({});
   const [, postData, isLoading] = useAxiosPost();
   const [uploadedImage, setUploadedImage] = useState([]);
+  const [employeeInfo, getEmployeeInfo] = useAxiosGet();
+
+  useEffect(() => {
+    getEmployeeInfo(
+      `/hcm/RemoteAttendance/GetEmployeeLoginInfo?AccountId=${accId}&BusinessUnitId=${buId}&EmployeeId=${employeeId}`
+    );
+  }, [accId, buId]);
 
   const saveHandler = (values, cb) => {
     const payload = {
@@ -70,6 +78,38 @@ export default function HomeBuildersInfoEntryForm() {
     );
   };
 
+  const initialValues = {
+    ...initData,
+    channel:
+      employeeInfo?.empLevelId === 7
+        ? {
+            value: employeeInfo?.empChannelId,
+            label: employeeInfo?.empChannelName,
+          }
+        : "",
+    region:
+      employeeInfo?.empLevelId === 7
+        ? {
+            value: employeeInfo?.regionId,
+            label: employeeInfo?.regionName,
+          }
+        : "",
+    area:
+      employeeInfo?.empLevelId === 7
+        ? {
+            value: employeeInfo?.areaId,
+            label: employeeInfo?.areaName,
+          }
+        : "",
+    territory:
+      employeeInfo?.empLevelId === 7
+        ? {
+            value: employeeInfo?.territoryInfoId,
+            label: employeeInfo?.territoryInfoName,
+          }
+        : "",
+  };
+
   return (
     <>
       {isLoading && <Loading />}
@@ -77,7 +117,7 @@ export default function HomeBuildersInfoEntryForm() {
         <Form
           {...objProps}
           viewType={type}
-          initData={initData}
+          initData={initialValues}
           saveHandler={saveHandler}
           setUploadedImage={setUploadedImage}
         />
