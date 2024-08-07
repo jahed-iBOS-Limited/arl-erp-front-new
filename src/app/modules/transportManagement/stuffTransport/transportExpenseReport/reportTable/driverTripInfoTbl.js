7f06a2a919e87ee0a43042b1adbce8f95f35b3da
rifat <rakibul.rifat@ibos.io>
2024-08-07 15:07:09 +0600

    refactor: sum 4 column value to total cost

--- src/app/modules/transportManagement/stuffTransport/transportExpenseReport/reportTable/driverTripInfoTbl.js
+++ src/app/modules/transportManagement/stuffTransport/transportExpenseReport/reportTable/driverTripInfoTbl.js
@@ -10,7 +10,7 @@ import AttachmentViewModal from "./attachmentViewModal";
 export default function DriverTripInfoTbl({ rowData }) {
   const [isShowModal, setIsShowModal] = useState(false);
   const [singleData, setSingleData] = useState({})
-  const [showAttachmentModal,setShowAttachmentModal] =useState(false)
+  const [showAttachmentModal, setShowAttachmentModal] = useState(false)
   const totalKM = rowData?.reduce((acc, curr) => acc + curr?.tripKM, 0);
   const totalTollAmount = rowData?.reduce(
     (acc, curr) => acc + curr?.numTollAmount,
@@ -38,114 +38,116 @@ export default function DriverTripInfoTbl({ rowData }) {
   const totalLPG = rowData?.reduce((acc, curr) => acc + curr?.lpeg, 0);
   const totalDiesel = rowData?.reduce((acc, curr) => (acc = curr?.diesel), 0);
   const totalOctane = rowData?.reduce((acc, curr) => (acc = curr?.octane), 0);
-  const totalFuelCost =totalCNG + totalLPG + totalDiesel + totalOctane;
+  const totalFuelCost = totalCNG + totalLPG + totalDiesel + totalOctane;
   return (
     <div>
       <h4 className="text-center mt-5">
         <strong>Driver Trip Details</strong>
       </h4>
       <div className="table-responsive">
-      <table className="table table-striped table-bordered bj-table bj-table-landing">
-        <thead>
-          <tr>
-            <th>SL</th>
-            <th>Date</th>
-            <th>Trip Type</th>
-            <th>Trip No</th>
-            <th>Start Mileage</th>
-            <th>End Milage</th>
-            <th>KM</th>
-            <th>Vehicle No</th>
-            <th>From</th>
-            <th>To</th>
-            <th>Toll</th>
-            <th>DA</th>
-            <th>Others</th>
-            <th>Maintaince</th>
-            <th>Total Driver Cost</th>
-            <th>CNG</th>
-            <th>LPG Gas</th>
-            <th>Diesel</th>
-            <th>Octane </th>
-            <th>Total Fuel Cost</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {rowData?.length > 0 &&
-            rowData?.map((item, index) => (
-              <tr key={index}>
-                <td className="text-center">{index + 1}</td>
-                <td className="text-center">
-                  {_dateFormatter(item?.dteTripDate)}
-                </td>
-                <td>{item?.strTripType}</td>
-                <td>{item?.strTripCode}</td>
-                <td className="text-center">{item?.startMileage}</td>
-                <td className="text-center">{item?.endMilage}</td>
-                <td className="text-center">{item?.tripKM}</td>
-                <td className="text-center">{item?.strVehicleNo}</td>
-                <td>{item?.strFirstRoundStartAddress}</td>
-                <td>{item?.strFirstRoundEndAddress}</td>
-                <td className="text-right">{item?.numTollAmount}</td>
-                <td className="text-right">{item?.numFoodAmount}</td>
-                <td className="text-right">{item?.numOthersAmount}</td>
-                <td className="text-right">{item?.numRepairingAmount}</td>
-                <td className="text-right">
-                  {item?.numTollAmount +
-                    item?.numOthersAmount +
-                    item?.numRepairingAmount}
-                </td>
-                <td className="text-right">{item?.cng}</td>
-                <td className="text-right">{item?.lpeg}</td>
-                <td className="text-right">{item?.diesel}</td>
-                <td className="text-right">{item?.octane}</td>
-                <td className="text-right">
-                  {item?.cng + item?.lpeg + item?.diesel + item?.octane}
-                </td>
-                <td className="text-center">
-                  <div style={{display:"flex", gap:"4px",padding:"0px 8px"}}>
-                  <span
-                    onClick={() => {
-                      setSingleData(item);
-                      setIsShowModal(true);
-                    }}
-                  >
-                    <IView styles={{fontSize:"15px"}}/>
-                  </span>
-                  <NewIcon
-                  customStyles={{cursor:"pointer",fontSize:"15px"}}
-                  title = "View All Attachment"
-                  clickHandler={()=>{
-                    setShowAttachmentModal(true)
-                    setSingleData(item);
-                  }}
-                  iconName="fa fa-file-image-o"
-                  />
-                  </div>
-                </td>
-              </tr>
-            ))}
-          {rowData?.length > 0 && (
-            <tr style={{ fontWeight: "bold" }}>
-              <td colSpan={6}>Total</td>
-              <td className="text-right"> {totalKM} </td>
-              <td colSpan={3}></td>
-              <td className="text-right">{_formatMoney(totalTollAmount)}</td>
-              <td className="text-right">{_formatMoney(totalFoodAmount)}</td>
-              <td className="text-right">{_formatMoney(totalOthers)}</td>
-              <td className="text-right">{_formatMoney(totalMaintenance)}</td>
-              <td className="text-right">{_formatMoney(totalDriverCost)}</td>
-              <td className="text-right">{_formatMoney(totalCNG)}</td>
-              <td className="text-right">{_formatMoney(totalLPG)}</td>
-              <td className="text-right">{_formatMoney(totalDiesel)}</td>
-              <td className="text-right">{_formatMoney(totalOctane)}</td>
-              <td className="text-right">{_formatMoney(totalFuelCost)}</td>
-              <td></td>
+        <table className="table table-striped table-bordered bj-table bj-table-landing">
+          <thead>
+            <tr>
+              <th>SL</th>
+              <th>Date</th>
+              <th>Trip Type</th>
+              <th>Trip No</th>
+              <th>Start Mileage</th>
+              <th>End Milage</th>
+              <th>KM</th>
+              <th>Vehicle No</th>
+              <th>From</th>
+              <th>To</th>
+              <th>Toll</th>
+              <th>DA</th>
+              <th>Others</th>
+              <th>Maintaince</th>
+              <th>Total Driver Cost</th>
+              <th>CNG</th>
+              <th>LPG Gas</th>
+              <th>Diesel</th>
+              <th>Octane </th>
+              <th>Total Fuel Cost</th>
+              <th>Action</th>
             </tr>
-          )}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {rowData?.length > 0 &&
+              rowData?.map((item, index) => (
+                <tr key={index}>
+                  <td className="text-center">{index + 1}</td>
+                  <td className="text-center">
+                    {_dateFormatter(item?.dteTripDate)}
+                  </td>
+                  <td>{item?.strTripType}</td>
+                  <td>{item?.strTripCode}</td>
+                  <td className="text-center">{item?.startMileage}</td>
+                  <td className="text-center">{item?.endMilage}</td>
+                  <td className="text-center">{item?.tripKM}</td>
+                  <td className="text-center">{item?.strVehicleNo}</td>
+                  <td>{item?.strFirstRoundStartAddress}</td>
+                  <td>{item?.strFirstRoundEndAddress}</td>
+                  <td className="text-right">{item?.numTollAmount}</td>
+                  <td className="text-right">{item?.numFoodAmount}</td>
+                  <td className="text-right">{item?.numOthersAmount}</td>
+                  <td className="text-right">{item?.numRepairingAmount}</td>
+                  <td className="text-right">
+                    {item?.numTollAmount +
+                      item?.numFoodAmount +
+                      item?.numOthersAmount +
+                      item?.numRepairingAmount
+                    }
+                  </td>
+                  <td className="text-right">{item?.cng}</td>
+                  <td className="text-right">{item?.lpeg}</td>
+                  <td className="text-right">{item?.diesel}</td>
+                  <td className="text-right">{item?.octane}</td>
+                  <td className="text-right">
+                    {item?.cng + item?.lpeg + item?.diesel + item?.octane}
+                  </td>
+                  <td className="text-center">
+                    <div style={{ display: "flex", gap: "4px", padding: "0px 8px" }}>
+                      <span
+                        onClick={() => {
+                          setSingleData(item);
+                          setIsShowModal(true);
+                        }}
+                      >
+                        <IView styles={{ fontSize: "15px" }} />
+                      </span>
+                      <NewIcon
+                        customStyles={{ cursor: "pointer", fontSize: "15px" }}
+                        title="View All Attachment"
+                        clickHandler={() => {
+                          setShowAttachmentModal(true)
+                          setSingleData(item);
+                        }}
+                        iconName="fa fa-file-image-o"
+                      />
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            {rowData?.length > 0 && (
+              <tr style={{ fontWeight: "bold" }}>
+                <td colSpan={6}>Total</td>
+                <td className="text-right"> {totalKM} </td>
+                <td colSpan={3}></td>
+                <td className="text-right">{_formatMoney(totalTollAmount)}</td>
+                <td className="text-right">{_formatMoney(totalFoodAmount)}</td>
+                <td className="text-right">{_formatMoney(totalOthers)}</td>
+                <td className="text-right">{_formatMoney(totalMaintenance)}</td>
+                <td className="text-right">{_formatMoney(totalDriverCost)}</td>
+                <td className="text-right">{_formatMoney(totalCNG)}</td>
+                <td className="text-right">{_formatMoney(totalLPG)}</td>
+                <td className="text-right">{_formatMoney(totalDiesel)}</td>
+                <td className="text-right">{_formatMoney(totalOctane)}</td>
+                <td className="text-right">{_formatMoney(totalFuelCost)}</td>
+                <td></td>
+              </tr>
+            )}
+          </tbody>
+        </table>
       </div>
       <div>
         <IViewModal
