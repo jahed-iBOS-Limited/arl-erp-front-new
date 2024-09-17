aa0af700769be3bc03a6329372ed4dedcc852cb4
najmus2808 <najmus2808@gmail.com>
2024-09-17 15:14:47 +0600

    VesselNominationAcceptance done

--- src/app/PublicRoutes.js
+++ src/app/PublicRoutes.js
@@ -3,8 +3,14 @@ import DeadWeightCreate from "./modules/chartering/operation/deadWeight/create";
 import CreateDischargePort from "./modules/chartering/operation/dischargePort/create";
 import EDPALoadPortCreate from "./modules/chartering/operation/edpaLoadPort/create";
 import CreateonHireBunkerAndContionalSurvey from "./modules/chartering/operation/onHireBunkerAndContionalSurvey/create";
+import VesselNominationAcceptance from "./modules/chartering/operation/vesselNominationAcceptance/create";
 
 export const publicRouteList = [
+  {
+    path: "/chartering/operation/vesselNominationAcceptance/accept",
+    component: VesselNominationAcceptance,
+    exact: true,
+  },
   {
     path: "/chartering/operation/bunkerManagement/create",
     component: BunkerManagement,
