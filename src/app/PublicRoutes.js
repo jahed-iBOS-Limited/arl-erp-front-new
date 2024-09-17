f5ffec19cbab389db349954c38f30f8e102ac3c7
najmus2808 <najmus2808@gmail.com>
2024-09-17 11:12:54 +0600

    change routing and add ddl

--- src/app/PublicRoutes.js
+++ src/app/PublicRoutes.js
@@ -2,14 +2,8 @@ import DeadWeightCreate from "./modules/chartering/operation/deadWeight/create";
 import CreateDischargePort from "./modules/chartering/operation/dischargePort/create";
 import EDPALoadPortCreate from "./modules/chartering/operation/edpaLoadPort/create";
 import CreateonHireBunkerAndContionalSurvey from "./modules/chartering/operation/onHireBunkerAndContionalSurvey/create";
-import RecapCreate from "./modules/chartering/operation/recap/create";
 
 export const publicRouteList = [
-  {
-    path: "/chartering/operation/recap/create",
-    component: RecapCreate,
-    exact: true,
-  },
   {
     path: "/chartering/operation/epdaLoadPort/create",
     component: EDPALoadPortCreate,
--- src/app/modules/chartering/operation/operationPages.js
+++ src/app/modules/chartering/operation/operationPages.js
@@ -16,10 +16,10 @@ export function OperationPages() {
         to="/chartering/operation/dashboard"
       />
 
-      {/* <Route
+      <Route
         path="/chartering/operation/recap/create"
         component={RecapCreate}
-      /> */}
+      />
       <Route path="/chartering/operation/recap" component={Recap} />
       <Route
         path="/chartering/operation/epdaLoadPort"
