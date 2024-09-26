6e9bc5d8d75afce6efe8f85a23c95819fdf7128e
rifat <rakibul.rifat@ibos.io>
2024-09-26 11:28:56 +0600

    ci: audit report path with lazy suspense

--- src/app/modules/internalControl/internalAudits/internalAuditsPages.js
+++ src/app/modules/internalControl/internalAudits/internalAuditsPages.js
@@ -1,30 +1,38 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Redirect, Switch } from "react-router-dom";
-import { ContentRoute } from "../../../../_metronic/layout";
+import { ContentRoute, LayoutSplashScreen } from "../../../../_metronic/layout";
 import AuditSchedules from "./auditschedules";
 import AuditSchedulesEntry from "./auditschedules/entry";
 import AuditSchedulesView from "./auditschedules/view";
+const AuditReportPage = lazy(() => import("./auditReport/landing"));
 
 export function InternalAuditsPages() {
   return (
-    <Switch>
-      <Redirect
-        exact={true}
-        from="/internal-control/internalaudits"
-        to="/internal-control/internalaudits/auditschedules"
-      />
-       <ContentRoute
-        path="/internal-control/internalaudits/auditschedules/view"
-        component={AuditSchedulesView}
-      />
-      <ContentRoute
-        path="/internal-control/internalaudits/auditschedules/entry"
-        component={AuditSchedulesEntry}
-      />
-      <ContentRoute
-        path="/internal-control/internalaudits/auditschedules"
-        component={AuditSchedules}
-      />
-    </Switch>
+    <Suspense fallback={<LayoutSplashScreen />}>
+      <Switch>
+        <Redirect
+          exact={true}
+          from="/internal-control/internalaudits"
+          to="/internal-control/internalaudits/auditschedules"
+        />
+        <ContentRoute
+          path="/internal-control/internalaudits/auditschedules/view"
+          component={AuditSchedulesView}
+        />
+        <ContentRoute
+          path="/internal-control/internalaudits/auditschedules/entry"
+          component={AuditSchedulesEntry}
+        />
+        <ContentRoute
+          path="/internal-control/internalaudits/auditschedules"
+          component={AuditSchedules}
+        />
+        {/* Audit Report */}
+        <ContentRoute
+          path="/internal-control/internalaudits/auditreport"
+          component={AuditReportPage}
+        />
+      </Switch>
+    </Suspense>
   );
 }
