312eb54e0188d71635ceb845117768bc19e79217
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2024-11-17 18:05:23 +0600

    :bug: fixed the invoice print issue

--- src/app/App.js
+++ src/app/App.js
@@ -36,7 +36,7 @@ export const eProcurementBaseURL =
 // live-url: https://erp.peopledesk.io
 
 export const APIUrl =
-  process.env.NODE_ENV === "development" ? "https://erp.ibos.io" : origin;
+  process.env.NODE_ENV === "development" ? "https://deverp.ibos.io" : origin;
 Axios.defaults.baseURL = APIUrl;
 
 const App = ({ store, persistor, basename }) => {
