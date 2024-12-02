3c6cb35ac59b3a3d3844711356aa8f757601b460
najmus2808 <najmus2808@gmail.com>
2024-12-02 17:41:15 +0600

    ass difrence

--- src/app/modules/procurement/purchase-management/autoPRCalculation/forecastvsActual.js
+++ src/app/modules/procurement/purchase-management/autoPRCalculation/forecastvsActual.js
@@ -1,22 +1,13 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { Form, Formik } from "formik";
-import React, { useReducer, useState } from "react";
+import React from "react";
 import { shallowEqual, useSelector } from "react-redux";
 import IForm from "../../../_helper/_form";
 import InputField from "../../../_helper/_inputField";
 import Loading from "../../../_helper/_loading";
 import NewSelect from "../../../_helper/_select";
 import { _todayDate } from "../../../_helper/_todayDate";
-import IViewModal from "../../../_helper/_viewModal";
 import useAxiosGet from "../../../_helper/customHooks/useAxiosGet";
-import BreakDownModal from "./breakdownModal";
-import CommonItemDetailsModal from "./rawMaterialModals/commonItemDetailsModal";
-import {
-    commonItemInitialState,
-    commonItemReducer,
-} from "./rawMaterialModals/helper";
-import WarehouseStockModal from "./rawMaterialModals/warehouseStockModal";
-import RawMaterialAutoPRNewModalView from "./rawMaterialModalView";
 
 const initData = {
     businessUnit: "",
@@ -25,178 +16,150 @@ const initData = {
 };
 
 export default function ForecastVsActual() {
-
-    const { profileData, businessUnitList } = useSelector((state) => {
-        return state.authData;
-    }, shallowEqual);
-
-    const [singleRowData, setSingleRowData] = useState();
-    const [showModal, setShowModal] = useState(false);
-
-    const [showBreakdownModal, setShowBreakdownModal] = useState(false);
-    const [warehouseStockModalShow, setWarehouseStockModalShow] = useState(false);
-
-    // reducer
-    const [commonItemDetailsState, commonItemDetailsDispatch] = useReducer(
-        commonItemReducer,
-        commonItemInitialState
+    const { selectedBusinessUnit, businessUnitList } = useSelector(
+        (state) => state.authData,
+        shallowEqual
     );
 
-    console.log("singleRowData", singleRowData)
-
-
-
-    const [
-        landingData,
-        getLandingData,
-        loading,
-        setLandingData,
-    ] = useAxiosGet();
-
-
-
-    const saveHandler = (values, cb) => {
-
-
-
-    };
+    const [landingData, getLandingData, loading, setLandingData] = useAxiosGet();
 
     const getData = (values) => {
-        getLandingData(`/mes/MESReport/GetProductionForecastVsActual?BusinessUnitId=${values?.businessUnit?.value}&FromDate=${values?.fromDate}&ToDate=${values?.toDate}`)
+        getLandingData(
+            `/mes/MESReport/GetProductionForecastVsActual?BusinessUnitId=${values?.businessUnit?.value}&FromDate=${values?.fromDate}&ToDate=${values?.toDate}`
+        );
     };
 
+    const calculateTotals = (key) =>
+        landingData.reduce((sum, item) => sum + (+item[key] || 0), 0);
 
-
+    const calculateDifferenceTotal = () =>
+        landingData.reduce(
+            (sum, item) =>
+                sum + ((+item.forecastedQty || 0) - (+item.approveQty || 0)),
+            0
+        );
 
     return (
         <Formik
-            enableReinitialize={true}
-            initialValues={initData}
-            // validationSchema={{}}
-            onSubmit={(values, { setSubmitting, resetForm }) => {
-                saveHandler(values, () => {
-                    resetForm(initData);
-                });
+            enableReinitialize
+            initialValues={{
+                ...initData,
+                businessUnit: { value: selectedBusinessUnit?.value, label: selectedBusinessUnit?.label }
+            }}
+            onSubmit={(values, { resetForm }) => {
+                resetForm(initData);
             }}
         >
-            {({
-                handleSubmit,
-                resetForm,
-                values,
-                setFieldValue,
-                isValid,
-                errors,
-                touched,
-            }) => (
+            {({ values, setFieldValue }) => (
                 <>
-                    {(loading) && <Loading />}
+                    {loading && <Loading />}
                     <IForm
                         title="Forecast Vs Actual"
                         isHiddenReset
                         isHiddenBack
                         isHiddenSave
-
                     >
                         <Form>
-                            <>
-                                <div className="form-group  global-form row">
-                                    <div className="col-lg-3">
-                                        <NewSelect
-                                            name="businessUnit"
-                                            options={businessUnitList || []}
-                                            value={values?.businessUnit}
-                                            label="Business Unit"
-                                            onChange={(valueOption) => {
-                                                setFieldValue("businessUnit", valueOption || "");
-                                                setLandingData([]);
-                                            }}
-                                            errors={errors}
-                                            touched={touched}
-                                        />
-                                    </div>
-
-
-                                    <div className="col-lg-3">
-                                        <InputField
-                                            value={values?.fromDate}
-                                            label="From Date"
-                                            name="fromDate"
-                                            type="date"
-                                            onChange={(e) => {
-                                                setFieldValue("fromDate", e.target.value);
-                                                setLandingData([]);
-                                            }}
-                                        />
-                                    </div>
-                                    <div className="col-lg-3">
-                                        <InputField
-                                            value={values?.toDate}
-                                            label="To Date"
-                                            name="toDate"
-                                            type="date"
-                                            onChange={(e) => {
-                                                setFieldValue("toDate", e.target.value);
-                                                setLandingData([]);
-                                            }}
-                                            min={values?.fromDate}
-                                        />
-                                    </div>
-                                    <div>
-                                        <button
-                                            type="button"
-                                            onClick={() => {
-                                                getData(values);
-                                            }}
-                                            disabled={!values?.businessUnit}
-                                            className="btn btn-primary mt-5"
-                                        >
-                                            Show
-                                        </button>
-                                    </div>
+                            {/* Form Inputs */}
+                            <div className="form-group global-form row">
+                                <div className="col-lg-3">
+                                    <NewSelect
+                                        name="businessUnit"
+                                        options={businessUnitList || []}
+                                        value={values?.businessUnit}
+                                        label="Business Unit"
+                                        onChange={(valueOption) => {
+                                            setFieldValue("businessUnit", valueOption || "");
+                                            setLandingData([]);
+                                        }}
+                                    />
                                 </div>
-                                <div className="table-responsive">
-                                    <table className="table table-striped mt-2 table-bordered bj-table bj-table-landing">
-                                        <thead>
-                                            <tr>
-                                                <th>SL</th>
-                                                <th>Product Name</th>
-                                                <th>Item Code</th>
-                                                <th>Forecasted Quantity</th>
-                                                <th>Approved Quantity</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            {/* Dynamic data rows */}
-                                            {landingData?.length > 0 &&
-                                                landingData?.map((item, index) => (
-                                                    <tr key={index}>
-                                                        <td>{index + 1}</td>
-                                                        <td>{item.productName}</td>
-                                                        <td className="text-center">{item.itemCode}</td>
-                                                        <td className="text-center">{item.forecastedQty}</td>
-                                                        <td className="text-center">{item.approveQty}</td>
-                                                    </tr>
-                                                ))}
+                                <div className="col-lg-3">
+                                    <InputField
+                                        value={values?.fromDate}
+                                        label="From Date"
+                                        name="fromDate"
+                                        type="date"
+                                        onChange={(e) => {
+                                            setFieldValue("fromDate", e.target.value);
+                                            setLandingData([]);
+                                        }}
+                                    />
+                                </div>
+                                <div className="col-lg-3">
+                                    <InputField
+                                        value={values?.toDate}
+                                        label="To Date"
+                                        name="toDate"
+                                        type="date"
+                                        onChange={(e) => {
+                                            setFieldValue("toDate", e.target.value);
+                                            setLandingData([]);
+                                        }}
+                                        min={values?.fromDate}
+                                    />
+                                </div>
+                                <div>
+                                    <button
+                                        type="button"
+                                        onClick={() => getData(values)}
+                                        disabled={!values?.businessUnit}
+                                        className="btn btn-primary mt-5"
+                                    >
+                                        Show
+                                    </button>
+                                </div>
+                            </div>
 
-                                            {/* Total row */}
-                                            {landingData?.length > 0 && (
-                                                <tr>
-                                                    <td colSpan={3} className="text-end fw-bold">Total</td>
-                                                    <td className="text-center fw-bold">
-                                                        {landingData.reduce((sum, item) => sum + item.forecastedQty, 0)}
-                                                    </td>
-                                                    <td className="text-center fw-bold">
-                                                        {landingData.reduce((sum, item) => sum + item.approveQty, 0)}
+                            {/* Table */}
+                            <div className="table-responsive">
+                                <table className="table table-striped mt-2 table-bordered bj-table bj-table-landing">
+                                    <thead>
+                                        <tr>
+                                            <th>SL</th>
+                                            <th>Product Name</th>
+                                            <th>Item Code</th>
+                                            <th>Forecasted Quantity</th>
+                                            <th>Actual Quantity</th>
+                                            <th>Difference Quantity</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {/* Dynamic Data Rows */}
+                                        {landingData?.length > 0 &&
+                                            landingData.map((item, index) => (
+                                                <tr key={index}>
+                                                    <td>{index + 1}</td>
+                                                    <td>{item.productName}</td>
+                                                    <td className="text-center">{item.itemCode}</td>
+                                                    <td className="text-center">{item.forecastedQty}</td>
+                                                    <td className="text-center">{item.approveQty}</td>
+                                                    <td className="text-center">
+                                                        {(+item.forecastedQty || 0) - (+item.approveQty || 0)}
                                                     </td>
                                                 </tr>
-                                            )}
-                                        </tbody>
-                                    </table>
+                                            ))}
 
-
-                                </div>
-
-                            </>
+                                        {/* Total Row */}
+                                        {landingData?.length > 0 && (
+                                            <tr>
+                                                <td colSpan={3} className="text-end fw-bold">
+                                                    Total
+                                                </td>
+                                                <td className="text-center fw-bold">
+                                                    {calculateTotals("forecastedQty")}
+                                                </td>
+                                                <td className="text-center fw-bold">
+                                                    {calculateTotals("approveQty")}
+                                                </td>
+                                                <td className="text-center fw-bold">
+                                                    {calculateDifferenceTotal()}
+                                                </td>
+                                            </tr>
+                                        )}
+                                    </tbody>
+                                </table>
+                            </div>
                         </Form>
                     </IForm>
                 </>
