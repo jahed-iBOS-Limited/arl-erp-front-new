5df0c344b3a751cd952b8e83e7bb59fcfb90a0d0
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2024-12-25 13:21:33 +0600

    :zap: report added

---
+++ src/app/modules/humanCapitalManagement/report/ReportPages.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Redirect, Switch } from "react-router-dom";
+import { ContentRoute } from "../../../../_metronic/layout";
+import ExpenceReport from "./expenseReport";
+
+export function ReportPages() {
+  return (
+    <Switch>
+      <Redirect
+        exact={true}
+        from="/human-capital-management"
+        to="/human-capital-management/report"
+      />
+      {/*  Expense Report*/}
+      <ContentRoute
+        path="/human-capital-management/Report/expenseReport"
+        component={ExpenceReport}
+      />
+    </Switch>
+  );
+}
