7dd62ef3cff04e2790b8dee0df3f013a8e9678dc
abdulkadirshohan <abdulkadirshohan@gmail.com>
2025-01-02 20:07:49 +0600

    Sea air modal

--- src/app/modules/CargoManagement/operation/bookingList/SeaAirMasterBl/index.jsx
+++ src/app/modules/CargoManagement/operation/bookingList/SeaAirMasterBl/index.jsx
@@ -1,5 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
+import MasterHBAWModal from "../masterHAWBModal";
+import MasterHBLModal from "../masterHBLModal";
 
-export default function SeaAirMasterBL() {
-  return <div>SeaAirMasterBL</div>;
+export default function SeaAirMasterBL({
+  selectedRow,
+  isPrintView,
+  CB,
+  airMasterBlid,
+  sipMasterBlid,
+}) {
+  const [transportPlanningType, setTransportPlanningType] = useState("");
+  return (
+    <div>
+      <div className="col-lg-4 mb-2 mt-5">
+        <label className="mr-3">
+          <input
+            type="radio"
+            name="transportPlanningType"
+            checked={transportPlanningType.value === 1}
+            className="mr-1 pointer"
+            style={{ position: "relative", top: "2px" }}
+            onChange={(e) => {
+              setTransportPlanningType({
+                value: 1,
+                label: "Air",
+              });
+            }}
+            // disabled={
+            //   shipBookingRequestGetById?.transportPlanning
+            //     ?.transportPlanningModeId === 1
+            // }
+            required
+          />
+          Air
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="transportPlanningType"
+            checked={transportPlanningType.value === 2}
+            className="mr-1 pointer"
+            style={{ position: "relative", top: "2px" }}
+            onChange={(e) => {
+              setTransportPlanningType({
+                value: 2,
+                label: "Sea",
+              });
+            }}
+            // disabled={
+            //   shipBookingRequestGetById?.transportPlanning
+            //     ?.transportPlanningModeId === 2
+            // }
+            required
+          />
+          Sea
+        </label>
+      </div>
+      {transportPlanningType.value === 1 && (
+        <MasterHBAWModal
+          selectedRow={selectedRow}
+          isPrintView={isPrintView}
+          CB={CB}
+        />
+      )}
+      {transportPlanningType.value === 2 && (
+        <MasterHBLModal
+          selectedRow={selectedRow}
+          isPrintView={isPrintView}
+          CB={CB}
+        />
+      )}
+    </div>
+  );
 }
