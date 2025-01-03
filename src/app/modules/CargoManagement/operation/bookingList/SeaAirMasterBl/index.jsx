7ab39ce9f79e66750dafea51c9458abed1183eee
jahedhossain <jahed.w.dev@gmail.com>
2025-01-04 02:12:36 +0600

    Update transport planning type logic to conditionally render radio buttons based on master BL generation status

--- src/app/modules/CargoManagement/operation/bookingList/SeaAirMasterBl/index.jsx
+++ src/app/modules/CargoManagement/operation/bookingList/SeaAirMasterBl/index.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import MasterHBAWModal from "../masterHAWBModal";
-import MasterHBLModal from "../masterHBLModal";
+import React, { useState } from 'react';
+import MasterHBAWModal from '../masterHAWBModal';
+import MasterHBLModal from '../masterHBLModal';
 
 export default function SeaAirMasterBL({
   selectedRow,
@@ -9,52 +9,57 @@ export default function SeaAirMasterBL({
   airMasterBlid,
   sipMasterBlid,
 }) {
-  const [transportPlanningType, setTransportPlanningType] = useState("");
+  const [transportPlanningType, setTransportPlanningType] = useState('');
   return (
     <div>
       <div className="col-lg-4 mb-2 mt-5">
-        <label className="mr-3">
-          <input
-            type="radio"
-            name="transportPlanningType"
-            checked={transportPlanningType.value === 1}
-            className="mr-1 pointer"
-            style={{ position: "relative", top: "2px" }}
-            onChange={(e) => {
-              setTransportPlanningType({
-                value: 1,
-                label: "Air",
-              });
-            }}
-            // disabled={
-            //   shipBookingRequestGetById?.transportPlanning
-            //     ?.transportPlanningModeId === 1
-            // }
-            required
-          />
-          Air
-        </label>
-        <label>
-          <input
-            type="radio"
-            name="transportPlanningType"
-            checked={transportPlanningType.value === 2}
-            className="mr-1 pointer"
-            style={{ position: "relative", top: "2px" }}
-            onChange={(e) => {
-              setTransportPlanningType({
-                value: 2,
-                label: "Sea",
-              });
-            }}
-            // disabled={
-            //   shipBookingRequestGetById?.transportPlanning
-            //     ?.transportPlanningModeId === 2
-            // }
-            required
-          />
-          Sea
-        </label>
+        {!selectedRow?.[0]?.isAirmasterBlGenarate && (
+          <label className="mr-3">
+            <input
+              type="radio"
+              name="transportPlanningType"
+              checked={transportPlanningType.value === 1}
+              className="mr-1 pointer"
+              style={{ position: 'relative', top: '2px' }}
+              onChange={(e) => {
+                setTransportPlanningType({
+                  value: 1,
+                  label: 'Air',
+                });
+              }}
+              // disabled={
+              //   shipBookingRequestGetById?.transportPlanning
+              //     ?.transportPlanningModeId === 1
+              // }
+              required
+            />
+            Air
+          </label>
+        )}
+
+        {!selectedRow?.[0]?.isSeamasterBlGenarate && (
+          <label>
+            <input
+              type="radio"
+              name="transportPlanningType"
+              checked={transportPlanningType.value === 2}
+              className="mr-1 pointer"
+              style={{ position: 'relative', top: '2px' }}
+              onChange={(e) => {
+                setTransportPlanningType({
+                  value: 2,
+                  label: 'Sea',
+                });
+              }}
+              // disabled={
+              //   shipBookingRequestGetById?.transportPlanning
+              //     ?.transportPlanningModeId === 2
+              // }
+              required
+            />
+            Sea
+          </label>
+        )}
       </div>
       {transportPlanningType.value === 1 && (
         <MasterHBAWModal
