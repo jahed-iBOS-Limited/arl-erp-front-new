f228d8ec74306a3b042babcbcff5efa66934f760
abdulkadirshohan <abdulkadirshohan@gmail.com>
2025-01-08 18:03:49 +0600

    Customer Lead Generation added

---
+++ src/app/modules/CargoManagement/chaOperation/chaOperationPage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Redirect, Switch } from 'react-router-dom';
+import { ContentRoute } from '../../../../_metronic/layout';
+import ChaShipmentBooking from './chaShipmentBooking';
+import CreateChaShipmentBooking from './createChaShipmentBooking';
+
+export function ChaOperationPages() {
+  return (
+    <Switch>
+      <Redirect
+        exact={true}
+        from="/cargoManagement"
+        to="/cargoManagement/cha-operation"
+      />
+      <ContentRoute
+        path="/cargoManagement/cha-operation/cha-shipment-booking/create"
+        component={CreateChaShipmentBooking}
+      />
+      <ContentRoute
+        path="/cargoManagement/cha-operation/cha-shipment-booking"
+        component={ChaShipmentBooking}
+      />
+    </Switch>
+  );
+}
+export default ChaOperationPages;
