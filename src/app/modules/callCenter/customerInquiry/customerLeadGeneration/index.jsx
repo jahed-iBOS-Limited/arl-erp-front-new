31ef5393ac2ec23a60fd4495166378cb30c20490
abdulkadirshohan <abdulkadirshohan@gmail.com>
2025-01-12 16:11:43 +0600

    update

--- src/app/modules/callCenter/customerInquiry/customerLeadGeneration/index.jsx
+++ src/app/modules/callCenter/customerInquiry/customerLeadGeneration/index.jsx
@@ -5,9 +5,19 @@ import { Form, Formik } from "formik";
 import * as Yup from "yup";
 import InputField from "../../../_helper/_inputField";
 import NewSelect from "../../../_helper/_select";
+import useAxiosGet from "../../../_helper/customHooks/useAxiosGet";
+import SearchAsyncSelect from "../../../_helper/SearchAsyncSelect";
+import FormikError from "../../../_helper/_formikError";
+import axios from "axios";
+import { shallowEqual, useSelector } from "react-redux";
+import PaginationTable from "../../../_helper/_tablePagination";
+import Loading from "../../../_helper/_loading";
 
 const validationSchema = Yup.object().shape({
-  statusAndStage: Yup.string().required("Status & Stage  is required"),
+  statusAndStage: Yup.object().shape({
+    value: Yup.number().required("Status & Stage is required"),
+    label: Yup.string().required("Status & Stage is required"),
+  }),
   division: Yup.object().shape({
     value: Yup.number().required("Division is required"),
     label: Yup.string().required("Division is required"),
@@ -23,11 +33,79 @@ const validationSchema = Yup.object().shape({
   fromDate: Yup.string().required("From Date is required"),
   toDate: Yup.string().required("To Date is required"),
 });
+const STAGE_STATUS = [
+  { value: 1, label: "Suspect" },
+  { value: 2, label: "Prospect" },
+  { value: 3, label: "Lead" },
+  { value: 4, label: "Client" },
+  { value: 5, label: "Customer" },
+];
 
 export default function CustomerLeadGeneration() {
   let history = useHistory();
+  const {
+    profileData: { userId },
+    selectedBusinessUnit,
+  } = useSelector((state) => state?.authData || {}, shallowEqual);
+  const [divisionDDL, getDivisionDDL] = useAxiosGet();
+  const [districtDDL, getDistrictDDL] = useAxiosGet();
+  const [pageNo, setPageNo] = React.useState(0);
+  const [pageSize, setPageSize] = React.useState(15);
+
+  const [landingData, getLandingData, isLoading] = useAxiosGet();
+
   const formikRef = React.useRef(null);
-  const saveHandler = (values, cb) => {};
+  const saveHandler = (values) => {
+    commonLandingApi();
+  };
+  const commonLandingApi = (PageNo = pageNo, PageSize = pageSize) => {
+    const values = formikRef.current.values;
+    let querystring = "";
+    if (values?.statusAndStage?.value) {
+      querystring += `&stageStatus=${values?.statusAndStage?.label}`;
+    }
+    if (values?.division?.value) {
+      querystring += `&divisionId=${values?.division?.value}`;
+    }
+    if (values?.district?.value) {
+      querystring += `&districtId=${values?.district?.value}`;
+    }
+    if (values?.thana?.value) {
+      querystring += `&transportzoneId=${values?.thana?.value}`;
+    }
+    if (values?.fromDate) {
+      querystring += `&fromDate=${values?.fromDate}`;
+    }
+    if (values?.toDate) {
+      querystring += `&toDate=${values?.toDate}`;
+    }
+    getLandingData(
+      `/oms/SalesQuotation/GetCustomerAcquisitionPagination?userId=${userId}&businessUnitId=${selectedBusinessUnit?.value}&pageNo=${pageNo}&pageSize=${pageSize}${querystring}`,
+      (data) => {
+        console.log(data, "data");
+      }
+    );
+  };
+  const getDistrict = (divisionId) => {
+    getDistrictDDL(
+      `/oms/TerritoryInfo/GetDistrictDDL?countryId=18&divisionId=${divisionId}`
+    );
+  };
+  const loadThana = (v) => {
+    if (v?.length < 2) return [];
+    return axios
+      .get(
+        `/oms/SalesQuotation/GetUserWiseShipToPartnerAndZoneDDL?partName=TransportZoneDDL&userId=${userId}&businessUnitId=${selectedBusinessUnit?.value}&districtId=${formikRef.current.values?.district?.value}&search=${v}`
+      )
+      .then((res) => {
+        return res?.data;
+      });
+  };
+
+  React.useEffect(() => {
+    getDivisionDDL("/oms/TerritoryInfo/GetDivisionDDL?countryId=18");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <ICustomCard
@@ -44,6 +122,7 @@ export default function CustomerLeadGeneration() {
         formikRef.current.resetForm();
       }}
     >
+      {isLoading && <Loading />}
       <Formik
         enableReinitialize={true}
         initialValues={{
@@ -76,16 +155,18 @@ export default function CustomerLeadGeneration() {
                 }}
               >
                 <div className="form-group row global-form">
-                  {/* name */}
+                  {/* Status & Stage */}
                   <div className="col-lg-3">
-                    <InputField
-                      label="Status & Stage"
-                      type="text"
-                      name="statusAndStage"
+                    <NewSelect
+                      label={"Status & Stage"}
+                      options={STAGE_STATUS || []}
                       value={values?.statusAndStage}
-                      onChange={(e) => {
-                        setFieldValue("statusAndStage", e.target.value);
+                      name="statusAndStage"
+                      onChange={(valueOption) => {
+                        setFieldValue("statusAndStage", valueOption || "");
                       }}
+                      errors={errors}
+                      touched={touched}
                     />
                   </div>
 
@@ -93,11 +174,14 @@ export default function CustomerLeadGeneration() {
                   <div className="col-lg-3">
                     <NewSelect
                       label={"Division"}
-                      options={[]}
+                      options={divisionDDL || []}
                       value={values?.division}
                       name="division"
                       onChange={(valueOption) => {
                         setFieldValue("division", valueOption || "");
+                        setFieldValue("district", "");
+                        setFieldValue("thana", "");
+                        valueOption?.value && getDistrict(valueOption?.value);
                       }}
                       errors={errors}
                       touched={touched}
@@ -107,27 +191,37 @@ export default function CustomerLeadGeneration() {
                   <div className="col-lg-3">
                     <NewSelect
                       label={"District"}
-                      options={[]}
+                      options={districtDDL || []}
                       value={values?.district}
                       name="district"
                       onChange={(valueOption) => {
                         setFieldValue("district", valueOption || "");
+                        setFieldValue("thana", "");
                       }}
                       errors={errors}
                       touched={touched}
+                      isDisabled={values?.division?.value ? false : true}
                     />
                   </div>
                   {/* thana */}
                   <div className="col-lg-3">
-                    <NewSelect
-                      label={"Thana"}
-                      options={[]}
-                      value={values?.thana}
-                      name="thana"
-                      onChange={(valueOption) => {
-                        setFieldValue("thana", valueOption || "");
+                    <label>Thana</label>
+                    <SearchAsyncSelect
+                      selectedValue={values?.thana}
+                      isSearchIcon={true}
+                      handleChange={(valueOption) => {
+                        setFieldValue("thana", valueOption);
                       }}
+                      loadOptions={loadThana}
+                      isDisabled={
+                        values?.division?.value && values?.district?.value
+                          ? false
+                          : true
+                      }
+                    />
+                    <FormikError
                       errors={errors}
+                      name={"thana"}
                       touched={touched}
                     />
                   </div>
@@ -189,6 +283,20 @@ export default function CustomerLeadGeneration() {
           <tbody></tbody>
         </table>
       </div>
+      {landingData?.data?.length > 0 && (
+        <PaginationTable
+          count={landingData?.totalCount}
+          setPositionHandler={(pageNo, pageSize) => {
+            commonLandingApi(pageNo, pageSize);
+          }}
+          paginationState={{
+            pageNo,
+            setPageNo,
+            pageSize,
+            setPageSize,
+          }}
+        />
+      )}
     </ICustomCard>
   );
 }
