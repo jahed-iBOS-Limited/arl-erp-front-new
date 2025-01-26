3f1e3b2e66ff0cfae71718c1eaa794a97bcc84ab
abdulkadirshohan <abdulkadirshohan@gmail.com>
2025-01-26 13:00:29 +0600

    feat: add NotPermittedPage for restricted routes in CargoManagementPages

--- src/app/modules/CargoManagement/cargoManagementPages.js
+++ src/app/modules/CargoManagement/cargoManagementPages.js
@@ -2,6 +2,7 @@ import React, { Suspense } from "react";
 import { shallowEqual, useSelector } from "react-redux";
 import { Redirect, Switch } from "react-router-dom";
 import { ContentRoute, LayoutSplashScreen } from "../../../_metronic/layout";
+import NotPermittedPage from "../_helper/notPermitted/NotPermittedPage";
 import ChaOperationPages from "./chaOperation/chaOperationPage";
 import DeliveryAgentPages from "./configuration/configurationPages";
 import OperationPages from "./operation/operationPages";
@@ -50,7 +51,22 @@ export function CargoManagementPages() {
             from="/cargoManagement"
             to="/cargoManagement/operation"
           />
-
+          <ContentRoute
+            path="/cargoManagement/operation"
+            component={NotPermittedPage}
+          />
+          <ContentRoute
+            path="/cargoManagement/report"
+            component={NotPermittedPage}
+          />
+          <ContentRoute
+            path="/cargoManagement/configuration"
+            component={NotPermittedPage}
+          />
+          <ContentRoute
+            path="/cargoManagement/cha-operation"
+            component={NotPermittedPage}
+          />
         </Switch>
       </Suspense>
     )
