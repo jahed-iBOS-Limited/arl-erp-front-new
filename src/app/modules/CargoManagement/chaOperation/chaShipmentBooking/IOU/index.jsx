f9fe84c936d16b586606a3ca17877651cb3c266f
abdulkadirshohan <abdulkadirshohan@gmail.com>
2025-01-29 10:33:19 +0600

    init modal

---
+++ src/app/modules/CargoManagement/chaOperation/chaShipmentBooking/IOU/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function IOU() {
+  return <div>IOU</div>;
+}
---
+++ src/app/modules/CargoManagement/chaOperation/chaShipmentBooking/ServiceAndCharge/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ServiceAndCharge() {
+  return <div>ServiceAndCharge</div>;
+}
