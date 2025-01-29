2e20f5570afc1e2d4e1718c00109ec9bf36fae50
abdulkadirshohan <abdulkadirshohan@gmail.com>
2025-01-29 17:10:50 +0600

    fix layout

--- src/app/modules/salesManagement/orderManagement/damageEntry/form/partialChallanTable.js
+++ src/app/modules/salesManagement/orderManagement/damageEntry/form/partialChallanTable.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { _dateFormatter } from "../../../../_helper/_dateFormate";
-import InputField from "../../../../_helper/_inputField";
 import { toast } from "react-toastify";
+import { _dateFormatter } from "../../../../_helper/_dateFormate";
 import { _fixedPoint } from "../../../../_helper/_fixedPoint";
+import InputField from "../../../../_helper/_inputField";
 
 export default function PartialChallanTable({ obj }) {
   const { gridData, allSelect, selectedAll, setGridData } = obj;
@@ -13,7 +13,7 @@ export default function PartialChallanTable({ obj }) {
     _data[headerIndex]["rowData"][rowIndex][key] = value;
     setGridData(_data);
   };
-  
+
   let totalDeliveryQty = 0;
   let totalAmount = 0;
   let totalDamage = 0;
@@ -32,7 +32,7 @@ export default function PartialChallanTable({ obj }) {
                   type="checkbox"
                   value={selectedAll()}
                   checked={selectedAll()}
-                  onChange={() => {}}
+                  onChange={() => { }}
                 />
               </th>
               <th>SL</th>
@@ -48,102 +48,67 @@ export default function PartialChallanTable({ obj }) {
             </tr>
           </thead>
           <tbody>
-            {gridData?.map((item, index) => {
-              return (
-                <>
-                  <tr key={index}>
-                    <td
-                      onClick={() => {
-                        let _data = [...gridData];
-                        _data[index]["isSelected"] = !item.isSelected;
-                        setGridData(_data);
-                      }}
-                      className="text-center"
-                    >
-                      <input
-                        type="checkbox"
-                        value={item?.isSelected}
-                        checked={item?.isSelected}
-                        onChange={() => {}}
-                      />
-                    </td>
-                    <td rowSpan={item?.rowData?.length}> {index + 1}</td>
-                    <td rowSpan={item?.rowData?.length}>
-                      {" "}
-                      {item?.deliveryCode}
-                    </td>
-                    <td rowSpan={item?.rowData?.length}>
-                      {" "}
-                      {_dateFormatter(item?.deliveryDate)}
-                    </td>
-                    {item?.rowData?.map((element, rowIndex) => {
-                      totalDeliveryQty += element?.quantity;
-                      totalAmount += element?.amount;
-                      totalDamage += element?.returnQty;
-                      return (
+            {gridData?.map((item, index) => (
+              <>
+                {item?.rowData?.map((element, rowIndex) => {
+                  totalDeliveryQty += element?.quantity;
+                  totalAmount += element?.amount;
+                  totalDamage += element?.returnQty;
+                  return (
+                    <tr key={`${index}-${rowIndex}`}>
+                      {/* Merge SL, Challan No, and Delivery Date only in the first row of each item */}
+                      {rowIndex === 0 && (
                         <>
-                          <td>{element?.itemName}</td>
-                          <td className="text-right">
-                            {_fixedPoint(element?.itemPrice, true, 0)}
+                          <td rowSpan={item?.rowData?.length} className="text-center">
+                            <input
+                              type="checkbox"
+                              checked={item?.isSelected}
+                              onChange={() => {
+                                let _data = [...gridData];
+                                _data[index]["isSelected"] = !item.isSelected;
+                                setGridData(_data);
+                              }}
+                            />
                           </td>
-                          <td className="text-right">
-                            {_fixedPoint(element?.quantity, true)}
-                          </td>
-                          <td className="text-right">
-                            {_fixedPoint(element?.amount, true)}
-                          </td>
-                          <td className="text-right">
-                            {item?.isSelected ? (
-                              <InputField
-                                value={element?.returnQty || ""}
-                                name="returnQty"
-                                placeholder="Return qty"
-                                type="number"
-                                min={"0.2"}
-                                onChange={(e) => {
-                                  if (+e.target.value < 0){
-                                    return toast.warn("Return qty can not be negative")
-                                  }
-                                  dataChangeHandler(
-                                    index,
-                                    rowIndex,
-                                    "returnQty",
-                                    +e?.target?.value
-                                  );
-                                }}
-                                // onBlur={(e) => {
-                                //   if (+e?.target?.value > element?.quantity) {
-                                //     toast.warn(
-                                //       "Return qty can not be greater than delivery qty"
-                                //     );
-                                //   }
-
-                                //   if (+e?.target?.value < 0.2) {
-                                //     toast.warn(
-                                //       "Return qty can not be less than 0.2"
-                                //     );
-                                //   }
-                                // }}
-                              />
-                            ) : (
-                              element?.returnQty
-                            )}
-                          </td>
-                          <td className="text-right">
-                            {_fixedPoint(element?.quantity * (2 / 100), true)}
-                          </td>
-                          <td className="text-right">
-                            {_fixedPoint(
-                              element?.quantity - element?.returnQty
-                            )}
-                          </td>
+                          <td rowSpan={item?.rowData?.length}>{index + 1}</td>
+                          <td rowSpan={item?.rowData?.length}>{item?.deliveryCode}</td>
+                          <td rowSpan={item?.rowData?.length}>{_dateFormatter(item?.deliveryDate)}</td>
                         </>
-                      );
-                    })}
-                  </tr>
-                </>
-              );
-            })}
+                      )}
+
+                      {/* These columns should appear correctly per row */}
+                      <td>{element?.itemName}</td>
+                      <td className="text-right">{_fixedPoint(element?.itemPrice, true, 0)}</td>
+                      <td className="text-right">{_fixedPoint(element?.quantity, true)}</td>
+                      <td className="text-right">{_fixedPoint(element?.amount, true)}</td>
+                      <td className="text-right">
+                        {item?.isSelected ? (
+                          <InputField
+                            value={element?.returnQty || ""}
+                            name="returnQty"
+                            placeholder="Return qty"
+                            type="number"
+                            min="0.2"
+                            onChange={(e) => {
+                              if (+e.target.value < 0) {
+                                return toast.warn("Return qty cannot be negative");
+                              }
+                              dataChangeHandler(index, rowIndex, "returnQty", +e.target.value);
+                            }}
+                          />
+                        ) : (
+                          element?.returnQty
+                        )}
+                      </td>
+                      <td className="text-right">{_fixedPoint(element?.quantity * (2 / 100), true)}</td>
+                      <td className="text-right">{_fixedPoint(element?.quantity - element?.returnQty)}</td>
+                    </tr>
+                  )
+                })}
+              </>
+            )
+            )}
+
             <tr style={{ textAlign: "right", fontWeight: "bold" }}>
               <td colSpan={6} className="text-right">
                 <b>Total</b>
