6be7d0e65ab481f6af07eea594b9468b5e444c46
jahedhossain <jahed.w.dev@gmail.com>
2025-02-03 19:08:00 +0600

    refactor: implement encryption for persisted state in Redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,33 +4,30 @@ import { reduxBatch } from '@manaflair/redux-batch';
 import { persistStore, persistReducer, createTransform } from 'redux-persist';
 import { rootReducer, rootSaga } from './rootReducer';
 import storage from 'redux-persist/lib/storage';
-// import CryptoJS from 'crypto-js';
+import CryptoJS from 'crypto-js';
 
-// const encrypt = createTransform(
-//   (inboundState, key) => {
-//     if (!inboundState) return inboundState;
-//     const cryptedText = CryptoJS.AES.encrypt(
-//       JSON.stringify(inboundState),
-//       import.meta.env.REACT_APP_DISCOUNT_NAME,
-//     );
-//     return cryptedText.toString();
-//   },
-//   (outboundState, key) => {
-//     if (!outboundState) return outboundState;
-//     const bytes = CryptoJS.AES.decrypt(
-//       outboundState,
-//       import.meta.env.REACT_APP_DISCOUNT_NAME,
-//     );
-//     const decrypted = bytes.toString(CryptoJS.enc.Utf8);
-//     return JSON.parse(decrypted);
-//   },
-// );
+const encrypt = createTransform(
+  (inboundState, key) => {
+    if (!inboundState) return inboundState;
+    const cryptedText = CryptoJS.AES.encrypt(
+      JSON.stringify(inboundState),
+      '7061737323313233',
+    );
+    return cryptedText.toString();
+  },
+  (outboundState, key) => {
+    if (!outboundState) return outboundState;
+    const bytes = CryptoJS.AES.decrypt(outboundState, '7061737323313233');
+    const decrypted = bytes.toString(CryptoJS.enc.Utf8);
+    return JSON.parse(decrypted);
+  },
+);
 
 const persistConfig = {
   key: 'root',
   storage,
   whitelist: ['authData', 'localStorage', 'pwa'],
-  // transforms: [encrypt],
+  transforms: [encrypt],
 };
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
