68a1da843bdff6e20a9daf56c11804a9031edc3b
jahedhossain <jahed.w.dev@gmail.com>
2025-02-27 12:08:46 +0600

    refactor: update total calculation and improve UI labels in IOU component

--- src/app/modules/CargoManagement/chaOperation/chaShipmentBooking/IOU/index.jsx
+++ src/app/modules/CargoManagement/chaOperation/chaShipmentBooking/IOU/index.jsx
@@ -210,18 +210,11 @@ export default function IOU({ clickRowDto, CB }) {
         const total = (+b?.rate || 0) * (+b?.quantity || 0);
         return a + total;
       }, 0) || 0;
-
-    const prvTotalAmount =
-      prviousShippingHeadOfCharges?.reduce((a, b) => {
-        const total = (+b?.rate || 0) * (+b?.quantity || 0);
-        return a + total;
-      }, 0) || 0;
-
     const prvAdvanceAmount =
       +prviousShippingHeadOfCharges?.[0]?.advanceAmount || 0;
     // const advanceAmount = isFirstTime ? 0 : currentTotalAmount - prvTotalAmount;
     const advanceAmount = isFirstTime ? 0 : prvAdvanceAmount;
-    const grandTotal = currentTotalAmount;
+    const grandTotal = currentTotalAmount - advanceAmount;
     setTotalAmountObj({
       totalAmount: currentTotalAmount,
       advanceAmount,
@@ -637,7 +630,7 @@ export default function IOU({ clickRowDto, CB }) {
                 </tr>
                 <tr>
                   <td colSpan="5" style={totalStyle}>
-                    Total:
+                    Total (Payable):
                   </td>
                   <td style={cellStyle}>{totalAmountObj?.grandTotal}</td>
                 </tr>
@@ -649,7 +642,10 @@ export default function IOU({ clickRowDto, CB }) {
                       }}
                     >
                       {totalAmountObj?.grandTotal &&
-                        convertNumberToWords(totalAmountObj?.grandTotal || 0)}
+                        convertNumberToWords(
+                          totalAmountObj?.grandTotal || 0,
+                        )}{' '}
+                      Taka Only
                     </span>
                   </td>
                 </tr>
