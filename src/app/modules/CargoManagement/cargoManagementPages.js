bb9a387fb9f7871d1b6319e5ede5202a536c90b3
jahedhossain <jahed.w.dev@gmail.com>
2025-03-12 10:11:03 +0600

    feat: add air operation pages and integrate routing in CargoManagement

--- src/app/modules/CargoManagement/cargoManagementPages.js
+++ src/app/modules/CargoManagement/cargoManagementPages.js
@@ -7,68 +7,44 @@ import ChaOperationPages from './chaOperation/chaOperationPage';
 import DeliveryAgentPages from './configuration/configurationPages';
 import OperationPages from './ffOperation/operationPages';
 import ReportPages from './report/reportPages';
+import AirOperationPages from './airOperation/airOperationPages';
 
 export function CargoManagementPages() {
   const { selectedBusinessUnit } = useSelector(
     (state) => state?.authData || {},
     shallowEqual,
   );
-  if (selectedBusinessUnit?.value === 225) {
-    return (
-      <Suspense fallback={<LayoutSplashScreen />}>
-        <Switch>
-          <Redirect
-            exact={true}
-            from="/cargoManagement"
-            to="/cargoManagement/operation"
-          />
-          <ContentRoute
-            path="/cargoManagement/operation"
-            component={OperationPages}
-          />
-          <ContentRoute
-            path="/cargoManagement/report"
-            component={ReportPages}
-          />
-          <ContentRoute
-            path="/cargoManagement/configuration"
-            component={DeliveryAgentPages}
-          />
-          <ContentRoute
-            path="/cargoManagement/cha-operation"
-            component={ChaOperationPages}
-          />
-        </Switch>
-      </Suspense>
-    );
-  } else {
-    return (
-      <Suspense fallback={<LayoutSplashScreen />}>
-        <Switch>
-          <Redirect
-            exact={true}
-            from="/cargoManagement"
-            to="/cargoManagement/operation"
-          />
-          <ContentRoute
-            path="/cargoManagement/operation"
-            component={NotPermittedPage}
-          />
-          <ContentRoute
-            path="/cargoManagement/report"
-            component={NotPermittedPage}
-          />
-          <ContentRoute
-            path="/cargoManagement/configuration"
-            component={NotPermittedPage}
-          />
-          <ContentRoute
-            path="/cargoManagement/cha-operation"
-            component={NotPermittedPage}
-          />
-        </Switch>
-      </Suspense>
-    );
+
+  if (selectedBusinessUnit?.value !== 225) {
+    return <NotPermittedPage />;
   }
+  return (
+    <Suspense fallback={<LayoutSplashScreen />}>
+      <Switch>
+        <Redirect
+          exact={true}
+          from="/cargoManagement"
+          to="/cargoManagement/operation"
+        />
+        <ContentRoute
+          path="/cargoManagement/air-operation"
+          component={AirOperationPages}
+        />
+        <ContentRoute
+          path="/cargoManagement/operation"
+          component={OperationPages}
+        />
+        <ContentRoute path="/cargoManagement/report" component={ReportPages} />
+        <ContentRoute
+          path="/cargoManagement/configuration"
+          component={DeliveryAgentPages}
+        />
+        <ContentRoute
+          path="/cargoManagement/cha-operation"
+          component={ChaOperationPages}
+        />
+      </Switch>
+    </Suspense>
+  );
 }
 export default CargoManagementPages;
