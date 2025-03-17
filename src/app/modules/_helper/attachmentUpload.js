169d890650303ff0f3e3aa9f42926a4288879484
MD. Abdul Kader <abdulkadirshohan@gmail.com>
2025-03-17 11:10:31 +0600

    refactor: rename newAttachment_action to attachmentUpload and clean up related code

--- src/app/modules/_helper/attachmentUpload.js
+++ src/app/modules/_helper/attachmentUpload.js
@@ -24,31 +24,7 @@ export const attachmentUpload = async (attachment, setLoading) => {
   }
 };
 
-export const newAttachment_action = async (
-  attachment,
-  setUploadImageLoading,
-) => {
-  setUploadImageLoading && setUploadImageLoading(true);
-  let formData = new FormData();
-  attachment.forEach((file) => {
-    formData.append('files', file?.file);
-  });
-  try {
-    let { data } = await axios.post('/domain/Document/UploadFile', formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    });
-    // toast.success(res?.data?.message || "Submitted Successfully");
-    toast.success('Upload  successfully');
-    setUploadImageLoading && setUploadImageLoading(false);
-    return data;
-  } catch (error) {
-    toast.error('Document not upload');
-    setUploadImageLoading && setUploadImageLoading(false);
-    return [];
-  }
-};
+
 export const empAttachment_action = async (attachment, cb) => {
   let formData = new FormData();
   attachment.forEach((file) => {
