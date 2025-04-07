e954ad15cefb6889514a5ad12712f622bfe54770
jahedhossain <jahed.w.dev@gmail.com>
2025-04-07 10:55:21 +0600

    refactor: update AllReport component to use environment variable for Google API key; improve code formatting and readability

--- src/app/modules/selfService/allReport/index.jsx
+++ src/app/modules/selfService/allReport/index.jsx
@@ -1,22 +1,22 @@
-import { Form, Formik } from "formik";
-import React, { useEffect, useState } from "react";
-import AllReportSearchInput from "./allReportSearchInput";
+import { Form, Formik } from 'formik';
+import React, { useEffect, useState } from 'react';
+import AllReportSearchInput from './allReportSearchInput';
 // import { allSheetData } from "./data";
-import "./style.scss";
-import { convertKeysSpace, searchMatch } from "./utility";
-import ICustomCard from "./../../_helper/_customCard";
-import useGoogleSheets from "../../_helper/useGoogleSheets";
-import Loading from "../../_helper/_loading";
+import './style.scss';
+import { convertKeysSpace, searchMatch } from './utility';
+import ICustomCard from './../../_helper/_customCard';
+import useGoogleSheets from '../../_helper/useGoogleSheets';
+import Loading from '../../_helper/_loading';
 function AllReport() {
   const { data, loading, error } = useGoogleSheets({
-    apiKey: "AIzaSyDjCB38yw-s2HKE4Oex20o9p_4iCb8yOtg",
-    sheetId: "1v0yhOrpfls66_UI7-MJRGbA7gAYhULNX3-_v2pmMSaI",
+    apiKey: import.meta.env.REACT_APP_GOOGLE_API_KEY,
+    sheetId: '1v0yhOrpfls66_UI7-MJRGbA7gAYhULNX3-_v2pmMSaI',
     // sheetsOptions: [{ id: 'Form Responses 2' }],
   });
 
   const [allList, setAllList] = useState([]);
   const [renderList, setRenderList] = useState([]);
-  const [searchInput, setSearchInput] = useState("");
+  const [searchInput, setSearchInput] = useState('');
   useEffect(() => {
     try {
       const _allSheetData = JSON.parse(JSON.stringify(data));
@@ -25,7 +25,7 @@ function AllReport() {
       if (_allSheetData?.length > 0) {
         _allSheetData.forEach((item) => {
           const _data = item?.data || [];
-          if(notShowList.includes(item?.id)) return;
+          if (notShowList.includes(item?.id)) return;
           _data.forEach((item) => {
             const newObj = convertKeysSpace(item);
             if (newObj?.standardreportname) {
@@ -39,7 +39,7 @@ function AllReport() {
           _list.map((item) => [
             `${item?.datasource}-${item?.standardreportname}-${item?.process}`,
             item,
-          ])
+          ]),
         ).values(),
       ];
       setAllList(uniqueCombinations || []);
@@ -54,12 +54,12 @@ function AllReport() {
     setRenderList(result);
   };
 
-  console.log(error)
+  console.log(error);
 
   return (
-    <div className='AllReportWrapper'>
+    <div className="AllReportWrapper">
       {loading && <Loading />}
-      <ICustomCard title='SSOT Report'>
+      <ICustomCard title="SSOT Report">
         <Formik
           enableReinitialize={true}
           initialValues={{}}
@@ -75,11 +75,11 @@ function AllReport() {
             isValid,
           }) => (
             <>
-              <Form className='form form-label-left'>
-                <div className='row'>
-                  <div className='col-lg-9 mt-1'>
+              <Form className="form form-label-left">
+                <div className="row">
+                  <div className="col-lg-9 mt-1">
                     <AllReportSearchInput
-                      placeholder='Report Search'
+                      placeholder="Report Search"
                       paginationSearchHandler={(value) => {
                         searchHandler(value);
                       }}
@@ -94,15 +94,15 @@ function AllReport() {
                     setListRender(!listRender)
                   }}>Click</div> */}
                 </div>
-                <div className='row mt-1'>
-                  <div className='col-lg-12'>
+                <div className="row mt-1">
+                  <div className="col-lg-12">
                     <>
-                      {" "}
-                      <div className='table-responsive'>
-                        <table className='table table-striped table-bordered global-table mt-0'>
+                      {' '}
+                      <div className="table-responsive">
+                        <table className="table table-striped table-bordered global-table mt-0">
                           <thead>
                             <tr>
-                              <th style={{ width: "30px" }}>SL</th>
+                              <th style={{ width: '30px' }}>SL</th>
                               <th>Standard Report Name</th>
                               <th>Report Format Link (URL)</th>
                               <th>Process</th>
@@ -120,10 +120,10 @@ function AllReport() {
                                     <td
                                       onClick={() => {
                                         // new tab open
-                                        window.open(itm?.[`${url}`], "_blank");
+                                        window.open(itm?.[`${url}`], '_blank');
                                       }}
                                     >
-                                      <sapn className='link'>
+                                      <sapn className="link">
                                         {itm?.[`${url}`]}
                                       </sapn>
                                     </td>
@@ -149,53 +149,3 @@ function AllReport() {
 }
 
 export default AllReport;
-
-// <>
-//   <ul className='list-group'>
-//     {(rowsPerPage > 0
-//       ? renderList.slice(
-//           page * rowsPerPage,
-//           page * rowsPerPage + rowsPerPage
-//         )
-//       : renderList
-//     ).map((itm, idx) => {
-//       let url = `reportformatlink(url)`;
-//       let sl = page * rowsPerPage + idx + 1;
-//       return (
-//         <>
-//           <li>
-//             <p>
-//               {" "}
-//               <span>{sl}) </span>
-//               {itm?.standardreportname}
-//             </p>
-//             <div className='buttomBox'>
-//               {itm?.[`${url}`] && (
-//                 <div
-//                   className='box link'
-//                   onClick={() => {
-//                     // new tab open
-//                     window.open(
-//                       itm?.[`${url}`],
-//                       "_blank"
-//                     );
-//                   }}
-//                 >
-//                   {itm?.[`${url}`]}
-//                 </div>
-//               )}
-//               {itm?.process && (
-//                 <div className='box'>{itm?.process}</div>
-//               )}
-//               {itm?.datasource && (
-//                 <div className='box'>
-//                   {itm?.datasource}
-//                 </div>
-//               )}
-//             </div>
-//           </li>
-//         </>
-//       );
-//     })}
-//   </ul>
-// </>
