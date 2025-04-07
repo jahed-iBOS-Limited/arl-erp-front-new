a4aca1101a47cae9bbbc95ffa97536f239c5df3f
najmus2808 <najmus2808@gmail.com>
2025-04-07 09:49:31 +0600

    fix: ensure loading indicator displays correctly in TableRow component

--- src/app/modules/productionManagement/manufacturingExecutionSystem/billOfMaterial/Table/tableRow.jsx
+++ src/app/modules/productionManagement/manufacturingExecutionSystem/billOfMaterial/Table/tableRow.jsx
@@ -137,8 +137,8 @@ export function TableRow() {
       )}
     >
       <Formik>
-        {loading && <Loading />}
         <>
+          {loading && <Loading />}
           <div className="global-form">
             <div className="row">
               <div className="col-lg-4">
