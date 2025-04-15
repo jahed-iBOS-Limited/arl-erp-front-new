4e2c9ff027e354d794d6a24f62903c4c718aecdd
rifat <rakibul.rifat@ibos.io>
2025-04-15 16:19:11 +0600

    fix: create incentive provision journal

--- src/app/modules/financialManagement/financials/reconciliationJournal/table/tableHeader.jsx
+++ src/app/modules/financialManagement/financials/reconciliationJournal/table/tableHeader.jsx
@@ -427,7 +427,7 @@ const ReconciliationJournal = () => {
                         const [year, month] =
                           values?.monthYear?.split('-')?.map(Number) || [];
                         createIncentiveProvisionJounral(
-                          `/fino/Report/GetIncentiveProvisionLanding?businessUnitId=${selectedBusinessUnit?.value}&yearId=${year}&monthId=${month}&totalValue=${totalIncentiveProvisionQty * values?.totalAmount}&actionBy=${profileData?.userId}`,
+                          `/fino/Report/CreateIncentiveProvisionJournal?businessUnitId=${selectedBusinessUnit?.value}&yearId=${year}&monthId=${month}&totalValue=${totalIncentiveProvisionQty * values?.totalAmount}&actionBy=${profileData?.userId}`,
                           null,
                           (response) => {
                             // status code
