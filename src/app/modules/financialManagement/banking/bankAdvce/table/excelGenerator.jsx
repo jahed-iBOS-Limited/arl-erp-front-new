326d077fb62ea70dbea7b6c81ff10cf5ccc9a2a8
jahedhossain <jahed.w.dev@gmail.com>
2025-04-22 11:46:28 +0600

    refactor: update import paths for format files in excelGenerator

--- src/app/modules/financialManagement/banking/bankAdvce/table/excelGenerator.jsx
+++ src/app/modules/financialManagement/banking/bankAdvce/table/excelGenerator.jsx
@@ -1,9 +1,8 @@
-import { formatFive } from '../excel/format-05';
-
+import { formatFive } from '../../../financials/bankAdvce/excel/format-05';
 import { formatOne } from '../../../financials/bankAdvce/excel/format-01';
-import { formatFour } from '../excel/format-04';
+import { formatFour } from '../../../financials/bankAdvce/excel/format-04';
 import { formatSeven } from '../../../financials/bankAdvce/excel/format-07';
-import { formatSix } from '../excel/format-06';
+import { formatSix } from '../../../financials/bankAdvce/excel/format-06';
 import { formatThree } from '../../../financials/bankAdvce/excel/format-03';
 import { formatTwo } from '../../../financials/bankAdvce/excel/format-02';
 
